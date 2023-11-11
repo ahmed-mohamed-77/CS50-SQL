50d37b6da624df74b6e2bd2bea98e74de072a75b
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-11 14:56:55 +0200

    Week 0 normals

---
+++ normals/8.sql
@@ -0,0 +1,3 @@
+SELECT "latitude", "longitude", "0m" FROM normals
+ORDER BY "0m", "latitude"
+LIMIT 10;
---
+++ normals/9.sql
@@ -0,0 +1,5 @@
+SELECT "latitude", "longitude", "0m" FROM normals
+ORDER BY "0m" DESC, "latitude" ASC
+LIMIT 10;
+
+
