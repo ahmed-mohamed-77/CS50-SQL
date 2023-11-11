8ae500fd3b0b0461724401825f59d01b2b1874af
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-11 15:04:03 +0000

    Sat, Nov 11, 2023, 5:04 PM +02:00

--- players/9.sql
+++ players/9.sql
@@ -1,3 +1,3 @@
 /*
 write a SQL query to find the players who played their final game in the MLB in 2022. Sort the results alphabetically by first name, then by last name*/
-SELECT 
+SELECT CONCAT("first_name", ' ',"last_name) 
