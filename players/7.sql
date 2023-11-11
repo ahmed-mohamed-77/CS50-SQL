c016298bf6c034e80b242302d3952e6b30695336
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-11 15:13:33 +0000

    Sat, Nov 11, 2023, 5:13 PM +02:00

--- players/7.sql
+++ players/7.sql
@@ -1,3 +1,6 @@
-SELECT COUNT(*) , "bats" FROM players
+/*
+In 7.sql, write a SQL query to count the number of players who bat (or batted) right-handed and throw (or threw) left-handed, or vice versa.
+*/
+SELECT COUNT(*) , "bats", "throws" FROM players
 GROUP BY "bats"
-HAVING "bats" = 'R' OR "bats" = 'L';
+HAVING "bats" = 'R' OR "throws" = 'L';
