7ca881995d7f523ff68be751766974de16dd58d4
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-11 16:25:08 +0000

    Sat, Nov 11, 2023, 6:25 PM +02:00

--- players/7.sql
+++ players/7.sql
@@ -1,4 +1,3 @@
-SELECT COUNT(*) AS "PlayerCount", "bats", "throws"
+SELECT COUNT(*)
 FROM players
-WHERE ("bats" = 'R' AND "throws" = 'L') OR ("bats" = 'L' AND "throws" = 'R')
-GROUP BY "bats", "throws";
+WHERE ("bats" = 'R' AND "throws" = 'L') OR ("bats" = 'L' AND "throws" = 'R');
