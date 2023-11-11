ce3d24ea8baae8b8f601a6cef7a718bb81f06675
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-11 14:43:35 +0000

    Sat, Nov 11, 2023, 4:43 PM +02:00

---
+++ players/6.sql
@@ -0,0 +1,6 @@
+/*
+write a SQL query to return the first name, last name, and debut date of players born in Pittsburgh,
+Pennsylvania (PA). Sort the results first by debut date—from most recent to oldest—then alphabetically by first name, followed by last name.
+*/
+
+
