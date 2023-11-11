d6e159ea1fe297f314f8393b0b109fec93e685ee
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-11 15:08:52 +0000

    Sat, Nov 11, 2023, 5:08 PM +02:00

---
+++ players/10.sql
@@ -0,0 +1,7 @@
+/*
+write SQL query to answer a question of your choice. This query should:
+Make use of AS to rename a column
+Involve at least condition, using WHERE
+Sort by at least one column using ORDER BY
+*/
+SELECT CONCAT("first_name", ' ',"last_name") AS "Player_Name", 
--- players/9.sql
+++ players/9.sql
@@ -1,5 +1,3 @@
-/*
-write a SQL query to find the players who played their final game in the MLB in 2022. Sort the results alphabetically by first name, then by last name*/
 SELECT "first_name", "last_name" FROM players
 WHERE "final_game" LIKE '2022%'
 ORDER BY "first_name", "last_name";
