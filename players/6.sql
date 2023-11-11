245b5cf2d4aafd1a0ecd7fc95906767d1d7e63c1
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-11 16:21:01 +0000

    Sat, Nov 11, 2023, 6:21 PM +02:00

--- players/6.sql
+++ players/6.sql
@@ -1,2 +1,3 @@
 SELECT "first_name", "last_name", "debut" FROM players
-WHERE "birth_city" = 'Pittsburgh' AND "birth_state" = 'PA';
+WHERE "birth_city" = 'Pittsburgh' AND "birth_state" = 'PA'
+ORDER BY "debut", "first_name", "last_name";
