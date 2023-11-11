3b4dc3182988fadd733619d59aab51c50a6a147a
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-11 15:12:10 +0000

    Sat, Nov 11, 2023, 5:12 PM +02:00

--- players/10.sql
+++ players/10.sql
@@ -1,7 +1,5 @@
-/*
-write SQL query to answer a question of your choice. This query should:
-Make use of AS to rename a column
-Involve at least condition, using WHERE
-Sort by at least one column using ORDER BY
-*/
-SELECT CONCAT("first_name", ' ',"last_name") AS "Player_Name", 
+
+SELECT CONCAT("first_name", ' ',"last_name") AS "Player_Name", "final_game"
+FROM players
+WHERE "weight" >= 200
+ORDER BY "Player_Name";
