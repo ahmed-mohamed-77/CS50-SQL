ffe61c9c78983b912ebc30ef14c9e226d4175723
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 22:12:36 +0000

    Tue, Nov 14, 2023, 12:12 AM +02:00

--- moneyball/8.sql
+++ moneyball/8.sql
@@ -1,6 +1,8 @@
-SELECT salary  FROM players AS PL
-JOIN salaries AS S ON PL.id = S.playerid
+SELECT S.salary
+FROM players AS PL
+JOIN salaries AS S ON PL.id = S.player_id
 JOIN performances AS P ON PL.id = P.player_id
-WHERE max(COUNT(P.H)) AND P.year = 2001
-ORDER BY salary DESC
+WHERE P.H = (SELECT MAX(H) FROM performances WHERE year = 2001)
+  AND P.year = 2001
+ORDER BY S.salary DESC
 LIMIT 1;
