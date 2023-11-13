11c70cbca2b91f2e74d92de00dc833a60ff1d71c
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 17:51:41 +0000

    Mon, Nov 13, 2023, 7:51 PM +02:00

--- dese/10.sql
+++ dese/10.sql
@@ -2,5 +2,5 @@ SELECT D.name, E.per_pupil_expenditure FROM districts AS D
 INNER JOIN expenditures AS E
 ON D.id = E.district_id
 WHERE D.type LIKE 'Public School%'
-ORDER BY E.pupils DESC
+ORDER BY D.name DESC
 LIMIT 10;
