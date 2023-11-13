3360f2e2bf51d8699d84c80eafbc463ed768ab67
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 21:29:31 +0000

    Mon, Nov 13, 2023, 11:29 PM +02:00

--- moneyball/3.sql
+++ moneyball/3.sql
@@ -1,9 +1,10 @@
 SELECT "year", "HR" FROM  performances
 WHERE "player_id" = (
-    SELECT "id", "first_name" , "last_name" FROM players
-    WHERE "first_name" = 'Ken' AND  "last_name" = 'Griffey'
-ORDER BY "year" DESC
+    SELECT "id"FROM players
+    WHERE ("first_name" = 'Ken' AND  "last_name" = 'Griffey')
+        AND "birth_year" = '1969'
 )
+ORDER BY "year" DESC
 
     SELECT "id", "first_name" , "last_name" FROM players
     WHERE ("first_name" = 'Ken' AND  "last_name" = 'Griffey')
