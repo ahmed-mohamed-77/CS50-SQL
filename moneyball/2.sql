4d1e25d3509671137f3b96e407fbdb0bebdbb577
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 21:07:12 +0000

    Mon, Nov 13, 2023, 11:07 PM +02:00

--- moneyball/2.sql
+++ moneyball/2.sql
@@ -1,6 +1,10 @@
 SELECT  ROUND(AVG(salary), 2) AS "average salary",	"year"
 FROM salaries AS S
 INNER JOIN players AS P ON P.id = S.player_id
-WHERE first_name + ' ' + last_name ='Cal Ripken Jr'
+WHERE P.first_name + ' ' + P.last_name ='Cal Ripken Jr'
 GROUP BY "year"
 ORDER BY "year" DESC;
+
+
+SELECT first_name, last_name FROM players
+WHERE first_name LIKE '%cal%';
