828e0e01a0eaaa50f4107c7c57611076289a9cd3
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 17:20:45 +0000

    Mon, Nov 13, 2023, 7:20 PM +02:00

--- dese/11.sql
+++ dese/11.sql
@@ -1,4 +1,5 @@
-SELECT name, pupils, per_pupil_expenditure, graduated
+SELECT S.name, E.per_pupil_expenditure, G.graduated
 FROM districts AS D
 INNER JOIN expenditures AS E ON D.id = E.district_id
-INNER JOIN schools AS E ON D.id = S.district_id
+INNER JOIN schools AS S ON D.id = S.district_id
+INNER JOIN graduation_rates AS G ON S.id = G.school_id;
