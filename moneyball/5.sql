1a669ea286946fdb055ce914df9ea4f53f83641e
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 21:47:45 +0000

    Mon, Nov 13, 2023, 11:47 PM +02:00

--- moneyball/5.sql
+++ moneyball/5.sql
@@ -1,6 +1,6 @@
 SELECT T.name FROM teams AS T
 JOIN performances AS P ON T.id = P.team_id
-JOIN 
+JOIN players AS P ON 
     SELECT "id" FROM players
     WHERE ("first_name" = 'Satchel' AND  "last_name" = 'Paige')
 
