f87a311eff683984098b89a4f0dce3ce86260289
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 17:34:03 +0000

    Mon, Nov 13, 2023, 7:34 PM +02:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,1 +1,5 @@
-SELECT name , per_pupil_expenditure, 
+SELECT D.name , E.per_pupil_expenditure, S.exemplary FROM districts AS D
+INNER JOIN expenditures AS E ON D.id = E.district_id
+INNER JOIN staff_evaluations AS S ON D.id = S.district_id
+ORDER BY S.exemplary DESC, E.per_pupil_expenditure DESC;
+
