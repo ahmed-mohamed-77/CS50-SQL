43656698110ecf40e6c61f17244017f0f79c6a4b
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 20:51:20 +0000

    Mon, Nov 13, 2023, 10:51 PM +02:00

---
+++ moneyball/1.sql
@@ -0,0 +1,8 @@
+/*
+
+You should start by getting a sense for how average player salaries have changed over time. In 1.sql, write a SQL query to find the average player salary by year.
+
+Sort by year in descending order.
+Round the salary to two decimal places and call the column “average salary”.
+Your query should return a table with two columns, one for year and one for average salary.
+*/
