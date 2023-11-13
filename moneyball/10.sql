94b3e2d6d373464629902414c4f6a6a708794e0d
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 22:30:39 +0000

    Tue, Nov 14, 2023, 12:30 AM +02:00

--- moneyball/10.sql
+++ moneyball/10.sql
@@ -1,5 +1,5 @@
-SELECT PL.first_name, PL.last_name, S.salary, P.HR, S.YEAR FROM players AS PL
+SELECT PL.first_name, PL.last_name, S.salary, P.HR, S.year FROM players AS PL
 JOIN salaries AS S ON PL.id = S.player_id
 JOIN performances AS P ON PL.id = P.player_id;
 ORDER BY P.player_id, S.year DESC, P.HR DESC, S.salary DESC
-
+WHERE S.year = P.year;
