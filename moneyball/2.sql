2ec7c376663c447a05f3bead74937904aa62a370
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 21:01:26 +0000

    Mon, Nov 13, 2023, 11:01 PM +02:00

--- moneyball/2.sql
+++ moneyball/2.sql
@@ -1,3 +1,5 @@
-SELECT  ROUND(AVG(salary), 2) AS "average salary",	"year" FROM salaries
-WHERE 
+SELECT  ROUND(AVG(salary), 2) AS "average salary",	"year"
+FROM salaries AS S
+INNER JOIN players AS P ON 
+WHERE
 ORDER BY "year" DESC;
