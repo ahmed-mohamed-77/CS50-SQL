87ad82a4474b5d6178647c4979955206e50470f2
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 21:38:30 +0000

    Mon, Nov 13, 2023, 11:38 PM +02:00

--- moneyball/4.sql
+++ moneyball/4.sql
@@ -1,5 +1,5 @@
 SELECT P.first_name, P.last_name, S.salary FROM players AS P
 JOIN salaries AS S ON P.id = S.player_id
 WHERE S.year = 2001
-ORDER BY S.salary
+ORDER BY S.salary, P.first_name
 LIMIT 50;
