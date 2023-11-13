c0230ad6658ecd52bf5139dfa6da8bc2e3b367e5
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 11:10:43 +0000

    Mon, Nov 13, 2023, 1:10 PM +02:00

--- packages/log.sql
+++ packages/log.sql
@@ -1,6 +1,12 @@
 
 -- *** The Lost Letter ***
 
+SELECT * FROM "addresses"
+WHERE "address" = '900 Somerville Avenue';
+
+SELECT * FROM "addresses"
+WHERE "address" = '2 Finnegan Street, uptown';
+
 -- *** The Devious Delivery ***
 
 -- *** The Forgotten Gift ***
