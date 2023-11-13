09aeeb4b635c9d6b3a9e9f4f3a629270b3b24752
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 18:09:49 +0000

    Mon, Nov 13, 2023, 8:09 PM +02:00

--- dese/6.sql
+++ dese/6.sql
@@ -1,6 +1,5 @@
-SELECT D.name, G.graduated FROM districts AS D
-INNER JOIN graduation_rates AS G
-ON D.id = G.school_id
-WHERE (D.type = 'Charter District' OR
-    D.type = 'Public School District')
-AND G.graduated = 100;
+SELECT "name" FROM schools
+WHERE "id" IN (
+    SELECT "school_id" FROM graduation_rates
+    WHERE "graduated" = 100
+);
