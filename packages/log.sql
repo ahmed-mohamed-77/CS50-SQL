95e69bd6c88f1438b8ffe5d32dadd2eeb3a470a5
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 11:18:43 +0000

    Mon, Nov 13, 2023, 1:18 PM +02:00

--- packages/log.sql
+++ packages/log.sql
@@ -1,11 +1,14 @@
 
 -- *** The Lost Letter ***
 
-SELECT * FROM "addresses"
+SELECT * FROM addresses
 WHERE "address" = '900 Somerville Avenue';
 
-SELECT * FROM "addresses"
+SELECT * FROM addresses
 WHERE "address" LIKE '2 Fin%';
+
+SELECT * FROM packages
+WHERE "from_address_id" = 432 AND "to_address_id" = 854;
 
 -- *** The Devious Delivery ***
 
