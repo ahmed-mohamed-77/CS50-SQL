697bf80816a59b0464891f476e5971bcc2c05a8d
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 17:42:18 +0000

    Mon, Nov 13, 2023, 7:42 PM +02:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,5 +1,8 @@
 SELECT D.name , E.per_pupil_expenditure, S.exemplary FROM districts AS D
 INNER JOIN expenditures AS E ON D.id = E.district_id
 INNER JOIN staff_evaluations AS S ON D.id = S.district_id
+WHERE 
 ORDER BY S.exemplary DESC, E.per_pupil_expenditure DESC;
 
+SELECT AVG(per_pupil_expenditure) FROM expenditures;
+SELECT AVG(exemplary) FROM staff_evaluations;
