c4c80e86b1501e3c2f2e3f3095b1b1725221e94d
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 17:45:40 +0000

    Mon, Nov 13, 2023, 7:45 PM +02:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,8 +1,7 @@
 SELECT D.name , E.per_pupil_expenditure, S.exemplary FROM districts AS D
 INNER JOIN expenditures AS E ON D.id = E.district_id
 INNER JOIN staff_evaluations AS S ON D.id = S.district_id
-WHERE 
+WHERE D.type LIKE '%Public School%' AND
+    E.per_pupil_expenditure >= (SELECT AVG(per_pupil_expenditure) FROM expenditures)
+    AND S.exemplary >= (SELECT AVG(exemplary) FROM staff_evaluations)
 ORDER BY S.exemplary DESC, E.per_pupil_expenditure DESC;
-
-SELECT AVG(per_pupil_expenditure) FROM expenditures;
-SELECT AVG(exemplary) FROM staff_evaluations;
