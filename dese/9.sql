0e89eae0afaca7d857ebf6bfe139569725980c4c
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 17:55:32 +0000

    Mon, Nov 13, 2023, 7:55 PM +02:00

--- dese/9.sql
+++ dese/9.sql
@@ -1,5 +1,4 @@
-SELECT D.name, E.per_pupil_expenditure FROM districts AS D
+SELECT D.name, E.pupil FROM districts AS D
 INNER JOIN expenditures AS E
 ON D.id = E.district_id
-GROUP BY D.name
-ORDER BY E.per_pupil_expenditure;
+ORDER BY E.pupil;
