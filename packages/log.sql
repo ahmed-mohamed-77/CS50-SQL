7a87a6d9cadcf67bb883ec7217bc7044de4085a9
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 11:29:57 +0000

    Mon, Nov 13, 2023, 1:29 PM +02:00

--- packages/log.sql
+++ packages/log.sql
@@ -12,7 +12,16 @@ AND "to_address_id" =
     (SELECT "id" FROM addresses
     WHERE "address" LIKE '2 Fin%');
 
-
+SELECT * FROM scans
+WHERE "address_id" = (
+    SELECT "id" FROM packages
+    WHERE "from_address_id" =(
+        SELECT "id" FROM addresses
+        WHERE "address" = '900 Somerville Avenue')
+    AND "to_address_id" =
+        (SELECT "id" FROM addresses
+        WHERE "address" LIKE '2 Fin%')
+);
 
 -- *** The Devious Delivery ***
 
