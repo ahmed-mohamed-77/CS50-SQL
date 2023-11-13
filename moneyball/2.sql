105ac6e302bf400cf2d955d894ba07efa2781700
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-13 21:13:04 +0000

    Mon, Nov 13, 2023, 11:13 PM +02:00

--- moneyball/2.sql
+++ moneyball/2.sql
@@ -1,10 +1,14 @@
 SELECT  ROUND(AVG(salary), 2) AS "average salary",	"year"
 FROM salaries AS S
 INNER JOIN players AS P ON P.id = S.player_id
-WHERE P.first_name + ' ' + P.last_name ='Cal Ripken Jr'
+WHERE P.first_name = 'cal' AND P.last_name ='Ripken'
 GROUP BY "year"
 ORDER BY "year" DESC;
 
-
-SELECT first_name, last_name FROM players
-WHERE first_name LIKE '%cal%';
+SELECT  ROUND(AVG(salary), 2) AS "average salary",	"year"
+FROM salaries
+WHERE "player_id" =
+    (SELECT id FROM players
+    WHERE first_name LIKE '%cal%' AND last_name LIKE '%Rip%')
+GROUP BY "year"
+ORDER BY "year" DESC;
