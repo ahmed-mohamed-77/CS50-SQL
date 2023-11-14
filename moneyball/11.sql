ea7409f1c2fc60090c357d7b7241200b8653cb98
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-14 21:39:54 +0000

    Tue, Nov 14, 2023, 11:39 PM +02:00

--- moneyball/11.sql
+++ moneyball/11.sql
@@ -1,4 +1,4 @@
 SELECT PL.first_name , PL.last_name , (S.salary / P.H) as "dollars per hit" FROM players AS PL
 JOIN salaries AS S ON PL.id = S.player_id
 JOIN performances AS P ON PL.id = P.player_id
-WHERE 
+WHERE P.year = 2001 AND "dollars per hit" IS NOT NULLL;
