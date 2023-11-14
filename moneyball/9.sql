43505e06d99b6246bef3a54a4cbb09455b738bb0
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-14 22:38:01 +0000

    Wed, Nov 15, 2023, 12:38 AM +02:00

--- moneyball/9.sql
+++ moneyball/9.sql
@@ -1,5 +1,7 @@
 SELECT 	T.name, ROUND(AVG(S.salary), 2) AS "average salary"
 FROM teams AS T
 JOIN salaries AS S ON T.id = S.team_id
+WHERE S.year = 2001
+GROUP BY T.name
 ORDER BY "average salary"
 LIMIT 5;
