f511b4d594e76cf7d53e4a5e98a2baa81ef6c15e
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-14 22:41:52 +0000

    Wed, Nov 15, 2023, 12:41 AM +02:00

--- moneyball/11.sql
+++ moneyball/11.sql
@@ -5,3 +5,21 @@ JOIN performances AS P ON PL.id = P.player_id
 WHERE (S.year AND P.year = 2001) AND P.H > 0
 ORDER BY "dollars per hit", PL.first_name , PL.last_name
 LIMIT 10;
+SELECT
+    PL.first_name,
+    PL.last_name,
+    CASE
+        WHEN P.H > 0 THEN ROUND(S.salary / P.H, 2)
+        ELSE NULL
+    END AS "dollars per hit"
+FROM
+    players AS PL
+JOIN
+    salaries AS S ON PL.id = S.player_id
+JOIN
+    performances AS P ON PL.id = P.player_id
+WHERE
+    S.year = 2001 AND P.year = 2001 AND P.H > 0
+ORDER BY
+    "dollars per hit"
+LIMIT 10;
