cde33f613e624d864f3e923aa4703c9ef930ed43
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-14 22:13:27 +0000

    Wed, Nov 15, 2023, 12:13 AM +02:00

--- moneyball/6.sql
+++ moneyball/6.sql
@@ -1,6 +1,6 @@
 SELECT T.name, SUM(P.H) AS "total hits" FROM teams AS T
 JOIN performances AS P ON T.id = P.team_id
 WHERE P.year = 2001
-GROUP BY P.player_id
+GROUP BY P.	team_id
 ORDER BY P.H DESC
 LIMIT 5;
