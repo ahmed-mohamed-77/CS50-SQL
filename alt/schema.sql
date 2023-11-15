dcbfd20ca7c0d7b7945c6cebdf068d2d39ac95a1
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-15 12:23:39 +0000

    Wed, Nov 15, 2023, 2:23 PM +02:00

--- alt/schema.sql
+++ alt/schema.sql
@@ -5,5 +5,11 @@ CREATE TABLE passengers(
     "age" INT NOT NULL,
 
     CONSTRAINT passinger_PK PRIMARY KEY ("id")
+);
+
+CREATE TABLE check-ins(
+    "id" INT NOT NULL,
+    "datetime" DATETIME NOT NULL,
+    
 )
 
