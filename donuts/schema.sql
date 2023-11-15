0ab997f55631e7adce5b6ebda464bb194f8007f5
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-15 19:40:17 +0000

    Wed, Nov 15, 2023, 9:40 PM +02:00

--- donuts/schema.sql
+++ donuts/schema.sql
@@ -1,4 +1,19 @@
 CREATE TABLE Ingredients(
     "id" INT,
-    
-)
+    "name" TEXT NOT NULL,
+    "unit" TEXT NOT NULL,
+    "price_per_unit" NUMERIC NOT NULL,
+
+    CONSTRAINT Ingredients_PK PRIMARY KEY("id")
+);
+
+CREATE TABLE Donuts(
+    "id" INT,
+    "ingredients_id" INT NOT NULL,
+    "name" TEXT NOT NULL,
+    "gluten_free" BOOLEAN NOT NULL,
+    "price_per_donuts" NUMERIC NOT NULL,
+
+    CONSTRAINT Donuts_PK PRIMARY KEY("id"),
+    CONSTRAINT Ingredients_FK FOREIGN KEY("ingredients_id") REFERENCES "Ingredients"("id")
+);
