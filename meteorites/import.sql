4f88a8953dda097ae9764795e62a08bf91ec9952
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-15 20:51:17 +0000

    Wed, Nov 15, 2023, 10:51 PM +02:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -1,12 +1,12 @@
 CREATE TABLE meteorites(
     "id" INT,
     "name" TEXT NOT NULL,
-    "class"
-    "mass" NUMERIC
-    "discovery"
+    "class" 
+    "mass" NUMERIC(5, 2)
+    "discovery" TEXT
     "year" INT
     "lat" NUMERIC(5, 2),
     "long" NUMERIC(5, 2)
 
-    
+
 )
