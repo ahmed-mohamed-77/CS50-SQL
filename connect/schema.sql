b7455c445af2621a0e8fd7c8d721d5d639e7eda6
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-15 19:14:49 +0000

    Wed, Nov 15, 2023, 9:14 PM +02:00

--- connect/schema.sql
+++ connect/schema.sql
@@ -58,5 +58,5 @@ CREATE TABLE companies_users(
 
     CONSTRAINT companiesuser_PK PRIMARY KEY ("user_id", "company_id"),
     CONSTRAINT companiesusers_users_FK FOREIGN KEY ("user_id") REFERENCES "users"("id"),
-    CONSTRAINT companiesusers_company_FK FOREIGN KEY "company_id" REFERENCES "companies"("id")
+    CONSTRAINT companiesusers_company_FK FOREIGN KEY ("company_id") REFERENCES "companies"("id")
 );
