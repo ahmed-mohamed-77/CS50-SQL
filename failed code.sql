4707cf77a8cbac07a5082a291cac31f6bb2f7391
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-15 13:04:09 +0000

    Wed, Nov 15, 2023, 3:04 PM +02:00

--- failed code.sql
+++ failed code.sql
@@ -14,3 +14,44 @@ WHERE P.H = (SELECT MAX(HR) FROM performances WHERE year = 2001)
 GROUP BY P.HR
 ORDER BY S.salary DESC
 LIMIT 1;
+
+-- sql moneyball sql-12
+/*
+  cannot fix even with chat-gpt
+  could not find the needed query
+*/
+WITH PlayerStats AS (
+    SELECT
+        PL.id AS player_id,
+        PL.first_name,
+        PL.last_name,
+        S.salary,
+        P.H,
+        P.RBI,
+        CASE
+            WHEN P.H > 0 THEN ROUND(S.salary / P.H, 2)
+            ELSE NULL
+        END AS "dollars per hit",
+        CASE
+            WHEN P.RBI > 0 THEN ROUND(S.salary / P.RBI, 2)
+            ELSE NULL
+        END AS "dollars per RBI"
+    FROM
+        players AS PL
+    JOIN
+        salaries AS S ON PL.id = S.player_id
+    JOIN
+        performances AS P ON PL.id = P.player_id
+    WHERE
+        S.year = 2001 AND P.year = 2001
+)
+SELECT
+    PS.first_name,
+    PS.last_name
+FROM
+    PlayerStats AS PS
+WHERE
+    PS.H > 0 AND PS.RBI > 0
+ORDER BY
+    PS.player_id
+LIMIT 10;
