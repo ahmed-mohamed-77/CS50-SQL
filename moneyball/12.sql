12186f16364aeefc44121381af0251b5d9116e41
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-15 11:29:50 +0000

    Wed, Nov 15, 2023, 1:29 PM +02:00

--- moneyball/12.sql
+++ moneyball/12.sql
@@ -1,10 +1,3 @@
-SELECT PL.first_name , PL.last_name , (S.salary / P.RBI) FROM players AS PL
-JOIN salaries AS S ON PL.id = S.player_id
-JOIN performances AS P ON PL.id = P.player_id
-WHERE (S.year AND P.year = 2001) AND P.RBI > 0
-ORDER BY PL.id , PL.last_name
-LIMIT 10;
-
 SELECT
     PL.first_name,
     PL.last_name,
