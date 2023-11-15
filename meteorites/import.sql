80d306f478157d22bafcf2a590c2fd0889257f0e
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-15 20:58:39 +0000

    Wed, Nov 15, 2023, 10:58 PM +02:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -1,12 +1,12 @@
 CREATE TABLE meteorites(
     "id" INT,
     "name" TEXT NOT NULL,
-    "class" TEXT
-    "mass" NUMERIC(5, 2)
-    "discovery" TEXT NOT NULL CHECK ("discovery" IN ('Fell', 'Found'))
-    "year" INT
+    "class" TEXT,
+    "mass" NUMERIC(5, 2),
+    "discovery" TEXT NOT NULL CHECK ("discovery" IN ('Fell', 'Found')),
+    "year" INT,
     "lat" NUMERIC(5, 2),
     "long" NUMERIC(5, 2),
 
     CONSTRAINT meteorites_PK PRIMARY KEY ("id")
-)
+);
