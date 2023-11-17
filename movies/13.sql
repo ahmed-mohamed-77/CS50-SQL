94e1b2acb92be05b9ccb5bf39d64e0b2d4d644e0
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-17 17:45:40 +0000

    Fri, Nov 17, 2023, 7:45 PM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,2 +1,14 @@
-SELECT * FROM people
-WHERE name = 'Kevin Bacon';
+WITH KevinBaconMovies AS (
+    SELECT DISTINCT m.id AS movie_id
+    FROM people p
+    JOIN stars s ON p.id = s.person_id
+    JOIN movies m ON s.movie_id = m.id
+    WHERE p.name = 'Kevin Bacon' AND p.birth = 1958
+)
+
+-- Step 2: Find all people who starred in the same movie(s) as Kevin Bacon
+SELECT DISTINCT p.name
+FROM people p
+JOIN stars s ON p.id = s.person_id
+JOIN KevinBaconMovies kbm ON s.movie_id = kbm.movie_id
+WHERE p.name != 'Kevin Bacon'; 
