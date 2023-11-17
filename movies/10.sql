ee7a47c709e1647263c49f4861c4f05920a211f2
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-17 17:33:05 +0000

    Fri, Nov 17, 2023, 7:33 PM +02:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,6 +1,5 @@
 SELECT DISTINCT P.name FROM movies AS M
 JOIN directors AS D ON M.id = D.movie_id
-JOIN people AS P ON P.id = S.person_id
-JOIN 
-WHERE M.year = 2004
-ORDER BY P.birth;
+JOIN people AS P ON P.id = D.person_id
+JOIN ratings AS R ON M.id =R.movie_id
+WHERE R.rating >= 9;
