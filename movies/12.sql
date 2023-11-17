c9333d8ef2f8de952c350fe0d928b9f53d7be072
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-17 17:50:18 +0000

    Fri, Nov 17, 2023, 7:50 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,5 +1,10 @@
-SELECT  distinct M.title FROM movies AS M
-JOIN stars AS S ON M.id = S.movie_id
-JOIN people AS P ON P.id = S.person_id
-JOIN ratings AS R ON M.id = R.movie_id
-WHERE P.name ='Bradley Cooper' OR P.name = 'Jennifer Lawrence';
+SELECT DISTINCT m.title
+FROM people p1
+JOIN stars s1 ON p1.id = s1.person_id
+JOIN movies m ON s1.movie_id = m.id
+JOIN stars s2 ON m.id = s2.movie_id
+JOIN people p2 ON s2.person_id = p2.id
+WHERE
+    (p1.name = 'Bradley Cooper' OR p2.name = 'Bradley Cooper')
+    AND (p1.name = 'Jennifer Lawrence' OR p2.name = 'Jennifer Lawrence')
+    AND p1.name <> p2.name;
