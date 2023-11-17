d59bf0b9e996b5f6b93337da7c6385c91f625275
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-17 17:30:17 +0000

    Fri, Nov 17, 2023, 7:30 PM +02:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,5 +1,6 @@
 SELECT DISTINCT P.name FROM movies AS M
-JOIN stars AS S ON M.id = S.movie_id
+JOIN directors AS D ON M.id = D.movie_id
 JOIN people AS P ON P.id = S.person_id
+JOIN 
 WHERE M.year = 2004
 ORDER BY P.birth;
