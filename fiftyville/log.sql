8c215303d808b04d95e2b0a9b03c4f93b66c54b1
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-17 14:10:49 +0000

    Fri, Nov 17, 2023, 4:10 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,5 @@
+SELECT "description" FROM crime_scene_reports
+WHERE "month" = 7 AND "day" = 28 AND "street" = 'Humphrey Street';
+
 SELECT * FROM crime_scene_reports
-WHERE 
+WHERE  "street" LIKE  'hump%';
