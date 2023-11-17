4cbd05c64b02f3e0e3321da33eec3e3708f8392c
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-17 17:20:48 +0000

    Fri, Nov 17, 2023, 7:20 PM +02:00

--- movies/8.sql
+++ movies/8.sql
@@ -4,11 +4,10 @@ You may assume that there is only one movie in the database with the title Toy S
 
 SELECT "name" FROM people
 WHERE "id" IN (
-    SELECT "person_id" FROM stars
-    WHERE "movie_id" = (
+    SELECT "person_id" FROM stars)
+    AND "movie_id" = (
         SELECT "id" FROM movies
         WHERE "title" = 'Toy Story'
-    )
 );
 
 
