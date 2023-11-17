b8ffb4f222c69399f77df446d331f38b7478ddec
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-17 17:49:46 +0000

    Fri, Nov 17, 2023, 7:49 PM +02:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,4 @@
-SELECT  M.title FROM movies AS M
+SELECT  distinct M.title FROM movies AS M
 JOIN stars AS S ON M.id = S.movie_id
 JOIN people AS P ON P.id = S.person_id
 JOIN ratings AS R ON M.id = R.movie_id
