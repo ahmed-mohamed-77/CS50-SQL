8147e0bdd22fed38188d85a9902ea0a71c05a197
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-17 14:15:42 +0000

    Fri, Nov 17, 2023, 4:15 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,3 +3,12 @@ WHERE "month" = 7 AND "day" = 28 AND "street" = 'Humphrey Street';
 
 SELECT * FROM crime_scene_reports
 WHERE  "street" LIKE  'hump%';
+
+
+/*Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery.
+Interviews were conducted today with three witnesses who were present at the time –
+each of their interview transcripts mentions the bakery.
+*/
+
+SELECT * FROM bakery_security_logs
+WHERE "month" = 7 ,"day" = 28 ,"hour" = 10 ,"minute" = 15 ;
