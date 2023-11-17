a5eb007be67f42114bbfc71241366f5703ebb4b5
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-17 17:40:18 +0000

    Fri, Nov 17, 2023, 7:40 PM +02:00

---
+++ movies/12.sql
@@ -0,0 +1,5 @@
+SELECT  M.title FROM movies AS M
+JOIN stars AS S ON M.id = S.movie_id
+JOIN people AS P ON P.id = S.person_id
+JOIN ratings AS R ON M.id = R.movie_id
+WHERE P.name ='Bradley Cooper' OR P.name = 'Jennifer Lawrence';
