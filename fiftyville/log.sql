dcc3eac453f2291978757653685488c6454d1d87
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-17 16:22:22 +0000

    Fri, Nov 17, 2023, 6:22 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -111,7 +111,7 @@ WHERE "id" = (
     SELECT destination_airport_id FROM flights
     WHERE "year" = 2021 AND  "month" = 7 AND "day" = 29
     AND "origin_airport_id" = (
-        SELECT "city" FROM airports
+        SELECT "id" FROM airports
         WHERE "city" = 'Fiftyville'
     )
     ORDER BY "hour", "minute"
