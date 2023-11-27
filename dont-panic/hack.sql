4487b14d46461d7fbc11386faee1e8a0f0c41c22
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-27 16:48:12 +0000

    Mon, Nov 27, 2023, 6:48 PM +02:00

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -8,3 +8,7 @@ Add false data to throw others off your trail. In particular,
 to frame emily33, make it only appear—in the user_logs table—as if the
 admin account has had its password changed to emily33’s password.
 */
+-- emily33 = cb4772eeda0ae562119b6c4be2e0889a
+
+INSERT INTO user_logs( "type", "old_username", "new_username", "old_password", "new_password")
+VALUES ('update', 'admin', 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'cb4772eeda0ae562119b6c4be2e0889a');
