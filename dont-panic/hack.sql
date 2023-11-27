4cd0cec2fb059835310cfebd35395090165096cf
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-27 16:27:00 +0000

    Mon, Nov 27, 2023, 6:27 PM +02:00

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -5,3 +5,9 @@ DELETE FROM user_logs WHERE id = (
     SELECT id FROM user_logs
     WHERE old_username = 'admin'
 );
+
+/*
+Add false data to throw others off your trail. In particular,
+to frame emily33, make it only appear—in the user_logs table—as if the
+admin account has had its password changed to emily33’s password.
+*/
