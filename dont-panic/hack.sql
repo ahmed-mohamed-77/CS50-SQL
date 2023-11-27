aadc37d9762150f55e1661b98c8530d4d0ce9482
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-27 16:58:44 +0000

    Mon, Nov 27, 2023, 6:58 PM +02:00

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -4,4 +4,5 @@ WHERE "id" = 1;
 DELETE FROM user_logs WHERE old_username = 'admin';
 
 INSERT INTO user_logs( "type", "old_username", "new_username", "old_password", "new_password")
-VALUES ('update', 'admin', 'emily33', 'NULL', 'cb4772eeda0ae562119b6c4be2e0889a');
+SELECT 'update', 'admin', 'admin', NULL, SELECT "password" FROM users
+WHERE username = 'emily33';
