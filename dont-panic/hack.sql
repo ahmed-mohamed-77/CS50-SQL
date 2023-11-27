8366343e66e6b2c5ba1275eb83c655c4bb54952d
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-27 16:39:22 +0000

    Mon, Nov 27, 2023, 6:39 PM +02:00

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -1,10 +1,7 @@
 UPDATE "users" SET password = '982c0381c279d139fd221fce974916e7'
 WHERE "id" = 1;
 
-DELETE FROM user_logs WHERE id = (
-    SELECT id FROM user_logs
-    WHERE old_username = 'admin'
-);
+DELETE FROM user_logs WHERE id = 'admin'
 
 /*
 Add false data to throw others off your trail. In particular,
