85270eb920ca1434220cb4c10a87e8d981af64c9
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-29 14:31:30 +0000

    Wed, Nov 29, 2023, 4:31 PM +02:00

--- bnb/one_bedrooms.sql
+++ bnb/one_bedrooms.sql
@@ -1,9 +1,4 @@
 CREATE VIEW one_bedrooms AS
 SELECT "id", "property_type", "host_name", "accommodates"
 FROM listings
-WHERE "accommodates" = 1
-LIMIT 25;
-
-
-
-DROP VIEW one_bedrooms;
+WHERE "accommodates" = 1;
