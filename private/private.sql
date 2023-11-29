3dcf76eff7555a0ccd49727179e239f599bfdea9
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-29 16:53:03 +0000

    Wed, Nov 29, 2023, 6:53 PM +02:00

--- private/private.sql
+++ private/private.sql
@@ -14,39 +14,7 @@ VALUES (14, 98, 4),
        (2230, 50, 7),
        (2346, 44, 10),
        (3041, 14, 5);
+
 CREATE VIEW message AS
 SELECT SUBSTR("sentence", "start_char", "lengh") AS "phrase" FROM triplets AS T
 JOIN sentences AS S ON S.id = T.sentence_id;
-
-CREATE VIEW message AS
-    SELECT SUBSTR("sentence", 98, 4) AS "encryption"
-    FROM "sentences"
-    WHERE id = 14;
-
-    SELECT SUBSTR("sentence", 3, 5) AS "encryption"
-    FROM "sentences"
-    WHERE id = 114;
-
-    SELECT SUBSTR("sentence", 72, 9) AS "encryption"
-    FROM "sentences"
-    WHERE id = 618;
-
-    SELECT SUBSTR("sentence", 7, 3) AS "encryption"
-    FROM "sentences"
-    WHERE id = 630;
-
-    SELECT SUBSTR("sentence", 12, 5) AS "encryption"
-    FROM "sentences"
-    WHERE id = 932;
-
-    SELECT SUBSTR("sentence", 50, 7) AS "encryption"
-    FROM "sentences"
-    WHERE id = 2230;
-
-    SELECT SUBSTR("sentence", 44, 10) AS "encryption"
-    FROM "sentences"
-    WHERE id = 2346;
-
-    SELECT SUBSTR("sentence", 14, 5) AS "encryption"
-    FROM "sentences"
-    WHERE id = 3041;
