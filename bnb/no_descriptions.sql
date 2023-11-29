c9817e3b45c91fe1f3e194580672d08df47444b6
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-29 12:30:03 +0000

    Wed, Nov 29, 2023, 2:30 PM +02:00

--- bnb/no_descriptions.sql
+++ bnb/no_descriptions.sql
@@ -1,7 +1,3 @@
 CREATE VIEW no_descriptions AS
-SELECT "id", "property_type" ,
-    "host_name" ,
-    "accommodates" INTEGER,
-    "bedrooms" INTEGER,
-    "description" ,
-    PRIMARY KEY("id")
+SELECT "id", "property_type", "host_name", "accommodates", "bedrooms"
+FROM listings;
