e932be594eb007853bfe7b4d76cc2e1bf3e59ed1
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-29 16:30:08 +0000

    Wed, Nov 29, 2023, 6:30 PM +02:00

--- private/private.sql
+++ private/private.sql
@@ -1,31 +1,32 @@
-SELECT SUBSTR("sentence", 98, 4) AS "encryption"
-FROM "sentences"
-WHERE id = 14;
+CREATE VIEW message AS
+    SELECT SUBSTR("sentence", 98, 4) AS "encryption"
+    FROM "sentences"
+    WHERE id = 14;
 
-SELECT SUBSTR("sentence", 3, 5) AS "encryption"
-FROM "sentences"
-WHERE id = 114;
+    SELECT SUBSTR("sentence", 3, 5) AS "encryption"
+    FROM "sentences"
+    WHERE id = 114;
 
-SELECT SUBSTR("sentence", 72, 9) AS "encryption"
-FROM "sentences"
-WHERE id = 618;
+    SELECT SUBSTR("sentence", 72, 9) AS "encryption"
+    FROM "sentences"
+    WHERE id = 618;
 
-SELECT SUBSTR("sentence", 7, 3) AS "encryption"
-FROM "sentences"
-WHERE id = 630;
+    SELECT SUBSTR("sentence", 7, 3) AS "encryption"
+    FROM "sentences"
+    WHERE id = 630;
 
-SELECT SUBSTR("sentence", 12, 5) AS "encryption"
-FROM "sentences"
-WHERE id = 932;
+    SELECT SUBSTR("sentence", 12, 5) AS "encryption"
+    FROM "sentences"
+    WHERE id = 932;
 
-SELECT SUBSTR("sentence", 50, 7) AS "encryption"
-FROM "sentences"
-WHERE id = 2230;
+    SELECT SUBSTR("sentence", 50, 7) AS "encryption"
+    FROM "sentences"
+    WHERE id = 2230;
 
-SELECT SUBSTR("sentence", 44, 10) AS "encryption"
-FROM "sentences"
-WHERE id = 2346;
+    SELECT SUBSTR("sentence", 44, 10) AS "encryption"
+    FROM "sentences"
+    WHERE id = 2346;
 
-SELECT SUBSTR("sentence", 14, 5) AS "encryption"
-FROM "sentences"
-WHERE id = 3041;
+    SELECT SUBSTR("sentence", 14, 5) AS "encryption"
+    FROM "sentences"
+    WHERE id = 3041;
