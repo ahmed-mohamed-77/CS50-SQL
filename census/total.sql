840fd42d62baefe1fba3f3d5379742618c98bd25
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-29 18:26:23 +0000

    Wed, Nov 29, 2023, 8:26 PM +02:00

--- census/total.sql
+++ census/total.sql
@@ -2,6 +2,4 @@ CREATE VIEW total AS
 SELECT
     SUM("families"), SUM("households"), SUM("population"), SUM("male"), SUM("female")
 FROM
-    census
-GROUP BY
-    "district", "locality";
+    census;
