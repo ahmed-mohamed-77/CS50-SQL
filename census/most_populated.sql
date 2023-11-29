0e7c8458f2214f9d9bcdf448eea8c626b2aa5443
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-29 18:13:38 +0000

    Wed, Nov 29, 2023, 8:13 PM +02:00

---
+++ census/most_populated.sql
@@ -0,0 +1,8 @@
+CREATE VIEW most_populated AS
+
+SELECT
+    "district", SUM("families"), SUM("households"), SUM("population"), SUM("male"), SUM("female")
+FROM
+    census
+GROUP BY
+    "district";
