8c22291eb8ca19018946b0027cb2bd9fdbb8960f
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-29 17:57:55 +0000

    Wed, Nov 29, 2023, 7:57 PM +02:00

--- census/rural.sql
+++ census/rural.sql
@@ -1,5 +1,4 @@
-
-
+CREATE VIEW rural AS
 SELECT "id", "district", "locality", "families","households","population","male","female"
 FROM census
 WHERE "locality" LIKE '%rural%';
---
+++ census/total.sql
@@ -0,0 +1,4 @@
+CREATE VIEW total AS
+
+
+
