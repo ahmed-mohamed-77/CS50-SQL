17b6b914251f66710431200849b4cedefd4c9df9
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-29 18:02:20 +0000

    Wed, Nov 29, 2023, 8:02 PM +02:00

--- census/total.sql
+++ census/total.sql
@@ -7,3 +7,12 @@ FROM
     census
 GROUP BY
     "district", "locality";
+
+
+SELECT
+    "district", "locality", SUM("families"), SUM("households"), SUM("population"), SUM("male"), SUM("female")
+FROM
+    census
+GROUP BY
+    "district", "locality"
+LIMIT 25;
