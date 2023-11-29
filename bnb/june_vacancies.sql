a1e97c9699ec22938a10db149f7eccf0eadcf990
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-29 15:24:44 +0000

    Wed, Nov 29, 2023, 5:24 PM +02:00

--- bnb/june_vacancies.sql
+++ bnb/june_vacancies.sql
@@ -1,5 +1,7 @@
 CREATE VIEW june_vacancies AS
-SELECT L."id", L."property_type", L."host_name", A."date" AS "days_vacant"
+SELECT
+L.id, L.property_type, L.host_name, A.date AS "days_vacant"
 FROM listings AS L
-JOIN availabilities AS A ON A."id" = L."listing_id"
-WHERE A."date" BETWEEN '2023-06-01' AND '2023-06-30';
+JOIN availabilities AS A ON A.id = L.listing_id
+WHERE (A.date BETWEEN '2023-06-01' AND '2023-06-30')
+AND A.available = "TRUE";
