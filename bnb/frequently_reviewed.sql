54e666ccce52031cb392549bcdae8ec11f2369d0
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-29 15:22:07 +0000

    Wed, Nov 29, 2023, 5:22 PM +02:00

--- bnb/frequently_reviewed.sql
+++ bnb/frequently_reviewed.sql
@@ -1,15 +1,14 @@
 CREATE VIEW frequently_reviewed AS
 SELECT
-    L."id",
-    L."property_type",
-    L."host_name",
-    COUNT(R."id") AS "reviews"
+    L.id,
+    L.property_type,
+    L.host_name
 FROM
     listings AS L
 JOIN
-    reviews AS R ON R."listing_id" = L."id"
+    reviews AS R ON R.listing_id = L.id
 GROUP BY
-    L."id", L."property_type", L."host_name"
+    L.id
 ORDER BY
-    "reviews" DESC, L."property_type", L."host_name"
+    COUNT(R.listing_id) DESC, L.property_type, L.host_name
 LIMIT 100;
