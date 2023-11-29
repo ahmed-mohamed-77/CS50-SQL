e8dc0f1c4b08b32723c71133dbeb544243f3167b
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-29 15:28:13 +0000

    Wed, Nov 29, 2023, 5:28 PM +02:00

--- bnb/june_vacancies.sql
+++ bnb/june_vacancies.sql
@@ -4,4 +4,5 @@ L.id, L.property_type, L.host_name, COUNT(A.date) AS "days_vacant"
 FROM listings AS L
 JOIN availabilities AS A ON L.id = A.listing_id
 WHERE (A.date LIKE'2023-06-%')
-AND A.available = "TRUE";
+AND A.available = "TRUE"
+GROUP BY L.id;
