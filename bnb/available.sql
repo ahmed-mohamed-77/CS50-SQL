035a852907b37f4e9be9afdf8a53240960b8b22d
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-29 14:53:20 +0000

    Wed, Nov 29, 2023, 4:53 PM +02:00

--- bnb/available.sql
+++ bnb/available.sql
@@ -1,4 +1,4 @@
 CREATE VIEW available AS
-SELECT L."id", L."property_type", L."host_name", A."date"
+SELECT L.id, L.property_type, L.host_name, A.date
 FROM listings AS L
-JOIN availabilities AS A ON A."id" = L."listing_id";
+JOIN availabilities AS A ON L.id = A.listing_id;
