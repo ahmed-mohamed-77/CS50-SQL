9dfdbbdddfe1e30459e04042c63c3f93c75125ca
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-29 18:10:05 +0000

    Wed, Nov 29, 2023, 8:10 PM +02:00

--- census/by_district.sql
+++ census/by_district.sql
@@ -1,4 +1,7 @@
 CREATE VIEW by_district AS
-
-
-SELECT district ,families, households, population, male, female
+SELECT
+    "district", SUM("families"), SUM("households"), SUM("population"), SUM("male"), SUM("female")
+FROM
+    census
+GROUP BY
+    "district";
