25c38310fe8dab26d3fcc35c0c83c6812589da18
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-11-29 16:52:07 +0000

    Wed, Nov 29, 2023, 6:52 PM +02:00

--- private/private.sql
+++ private/private.sql
@@ -14,8 +14,8 @@ VALUES (14, 98, 4),
        (2230, 50, 7),
        (2346, 44, 10),
        (3041, 14, 5);
-
-SELECT SUBSTR("sentence", "start_char", "lengh") FROM triplets AS T
+CREATE VIEW message AS
+SELECT SUBSTR("sentence", "start_char", "lengh") AS "phrase" FROM triplets AS T
 JOIN sentences AS S ON S.id = T.sentence_id;
 
 CREATE VIEW message AS
