f7502a9118daf317bb9d8cfdce3e57bb474ab574
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-03 13:17:03 +0000

    Sun, Dec 3, 2023, 3:17 PM +02:00

--- harvard/indexes.sql
+++ harvard/indexes.sql
@@ -1,1 +1,8 @@
 CREATE INDEX student_inrollment ON enrollments("student_id", "course_id");
+
+EXPLAIN QUREY PLAN
+SELECT "courses"."title", "courses"."semester"
+FROM "enrollments"
+JOIN "courses" ON "enrollments"."course_id" = "courses"."id"
+JOIN "students" ON "enrollments"."student_id" = "students"."id"
+WHERE "students"."id" = 3;
