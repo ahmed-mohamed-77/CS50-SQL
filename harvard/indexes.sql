1a82e118f44aa2a69eee4d10692b0f9f7d73a3e8
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-03 13:22:07 +0000

    Sun, Dec 3, 2023, 3:22 PM +02:00

--- harvard/indexes.sql
+++ harvard/indexes.sql
@@ -6,3 +6,7 @@ FROM "enrollments"
 JOIN "courses" ON "enrollments"."course_id" = "courses"."id"
 JOIN "students" ON "enrollments"."student_id" = "students"."id"
 WHERE "students"."id" = 3;
+
+CREATE INDEX course_name ON courses(id);
+CREATE INDEX student_name ON students(id);
+
