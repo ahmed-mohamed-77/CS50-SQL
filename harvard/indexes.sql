5ff29733ae32eaeb348e28fb06925bd83d9b3359
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-03 14:35:33 +0000

    Sun, Dec 3, 2023, 4:35 PM +02:00

--- harvard/indexes.sql
+++ harvard/indexes.sql
@@ -1,7 +1,11 @@
-CREATE INDEX student_inrollment ON enrollments("student_id", "course_id");
+CREATE INDEX student_inrollment ON
+    enrollments("student_id", "course_id");
 
-CREATE INDEX course_name ON courses("id", "department", "number", "semester");
+CREATE INDEX course_name ON
+    courses("id", "department", "number", "semester");
+
 CREATE INDEX student_name ON students("id", "name");
+
 CREATE INDEX course_inrollment ON enrollments("course_id");
 
 
