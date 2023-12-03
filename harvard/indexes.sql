f9253126b7075b14bcce28043063bfe3643ade4f
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-03 13:33:26 +0000

    Sun, Dec 3, 2023, 3:33 PM +02:00

--- harvard/indexes.sql
+++ harvard/indexes.sql
@@ -1,15 +1,7 @@
 CREATE INDEX student_inrollment ON enrollments("student_id", "course_id");
 
-EXPLAIN QUREY PLAN
-SELECT "courses"."title", "courses"."semester"
-FROM "enrollments"
-JOIN "courses" ON "enrollments"."course_id" = "courses"."id"
-JOIN "students" ON "enrollments"."student_id" = "students"."id"
-WHERE "students"."id" = 3;
-
 CREATE INDEX course_name ON courses("id", "department", "number", "semester");
 CREATE INDEX student_name ON students("id", "name");
+CREATE INDEX course_inrollment ON enrollments("course_id");
 
 
-DROP INDEX course_name;
-DROP INDEX student_name;
