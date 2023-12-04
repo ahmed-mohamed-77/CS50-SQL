2e727989eb8a65e5ebfb73a4834cc8e96306ce77
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-04 20:14:57 +0000

    Mon, Dec 4, 2023, 10:14 PM +02:00

--- project/queries.sql
+++ project/queries.sql
@@ -1,1 +1,2 @@
--- In this SQL file, write (and comment!) the typical SQL queries users will run on your database
+-- In this SQL file, write (and comment!) the typical SQL queries users will run
+-- on your database
--- project/schema.sql
+++ project/schema.sql
@@ -1,1 +1,3 @@
--- In this SQL file, write (and comment!) the schema of your database, including the CREATE TABLE, CREATE INDEX, CREATE VIEW, etc. statements that compose it
+-- In this SQL file, write (and comment!) the schema of your database,
+-- including the CREATE TABLE, CREATE INDEX, CREATE VIEW, etc.
+-- statements that compose it
