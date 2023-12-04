533d99f17fafb2808eabb9be0d058642def32d4c
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-04 19:24:13 +0000

    Mon, Dec 4, 2023, 9:24 PM +02:00

--- dont-panic-python/hack.py
+++ dont-panic-python/hack.py
@@ -2,7 +2,7 @@ from cs50 import SQL
 db = SQL("sqlite:////home/ubuntu/CS50-SQL/dont-panic-python/dont-panic.db")
 password = input("Enter a password: ")
 db.execute(
-    UPDATE users SET "password" = ?
+    UPDATE users SET "password" = password
     WHERE "username" = 'admin';
 )
 
