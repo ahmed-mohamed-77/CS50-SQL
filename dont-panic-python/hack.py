17f89f87b7890f376590cdc44ebc40839c0f6883
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-04 19:17:33 +0000

    Mon, Dec 4, 2023, 9:17 PM +02:00

--- dont-panic-python/hack.py
+++ dont-panic-python/hack.py
@@ -1,3 +1,8 @@
-import 
+from cs50 import SQL
+db = SQL("sqlite:////home/ubuntu/CS50-SQL/dont-panic-python/dont-panic.db")
+db.execute(
+    UPDATE users SET password = 'hacked!'
+    WHERE username = 'admin';
+)
 
 print("Hacked!")
