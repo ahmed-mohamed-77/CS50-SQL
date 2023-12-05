bc4ffa7117a00b71992de91f516d4e5a7b73b3b4
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-05 11:45:59 +0000

    Tue, Dec 5, 2023, 1:45 PM +02:00

--- project/schema.sql
+++ project/schema.sql
@@ -5,12 +5,21 @@
 
 CREATE TABLE people(
     "id" INT,
-    "employee_id"
     "first_name" VARCHAR(15) NOT NULL,
     "last_name" VARCHAR(15) NOT NULL,
     "email" VARCHAR(55) NOT NULL,
     "address" VARCHAR(60) NOT NULL,
     "phone" VARCHAR(15) NOT NULL,
     "role" VARCHAR(9) CHECK("role" = 'Customer' OR "role" = 'Employee') NOT NULL,
+);
 
+CREATE TABLE employee(
+    "employee_id" INT,
+    "branch_id" INT,
+    "position" VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE customers(
+    "customer_id" INT,
+    "customer_type" VARCHAR(25)
 )
