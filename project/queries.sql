db2b6a08431fbef7df31d67ea9d6cf0f3427c5ac
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-06 10:45:04 +0000

    Wed, Dec 6, 2023, 12:45 PM +02:00

--- project/queries.sql
+++ project/queries.sql
@@ -2,6 +2,10 @@
 -- write (and comment!) the typical SQL queries users will run
 -- on your database
 
+-- Add the new column
 ALTER TABLE branchs
-ADD COLUMN "people_id" INTEGER,
+ADD COLUMN "people_id" INTEGER;
+
+-- Add the foreign key constraint
+ALTER TABLE branchs
 ADD CONSTRAINT fk_people_id FOREIGN KEY ("people_id") REFERENCES peoples("id");
