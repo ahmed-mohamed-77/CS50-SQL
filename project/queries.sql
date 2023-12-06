df0d7ca23037636987ec9eb77a9997a92aa8d9ba
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2023-12-06 10:47:22 +0000

    Wed, Dec 6, 2023, 12:47 PM +02:00

--- project/queries.sql
+++ project/queries.sql
@@ -9,3 +9,6 @@ ADD COLUMN "people_id" INTEGER;
 -- Add the foreign key constraint
 ALTER TABLE branchs
 ADD CONSTRAINT fk_people_id FOREIGN KEY ("people_id") REFERENCES peoples("id");
+
+ALTER TABLE branchs
+ADD  FOREIGN KEY ("people_id") REFERENCES peoples("id");
