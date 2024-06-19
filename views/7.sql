ab02dc1130073768ac9a050c71369c0dc7558a45
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 19:58:18 +0000

    Wed, Jun 19, 2024, 10:58 PM +03:00

--- views/7.sql
+++ views/7.sql
@@ -3,4 +3,6 @@ Hiroshige, from most to least bright. Compare them to this list on Wikipedia to 
 if your results match the print’s aesthetics.
 Executing 7.sql results in a table with 1 column and 5 rows
 
-SELECT english_title
+SELECT english_title FROM
+WHERE artist = 'Hiroshige'
+ORDER BY 
