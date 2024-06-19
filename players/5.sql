0c15e98379e23d1e62502cc8447a446c5876a1ac
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 15:35:37 +0000

    Wed, Jun 19, 2024, 6:35 PM +03:00

--- players/5.sql
+++ players/5.sql
@@ -1,1 +1,4 @@
-In 5.sql, write a SQL query to return the first and last names of all right-handed batters. Sort the results alphabetically by first name, then by last name.
+In 5.sql, write a SQL query to return the first and last names of all right-handed batters.
+Sort the results alphabetically by first name, then by last name.
+
+SELECT first_name, last_name
