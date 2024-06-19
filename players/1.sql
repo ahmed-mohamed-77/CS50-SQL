fc58760cb07daf581f6291f867cdf803016fad4c
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 15:13:05 +0000

    Wed, Jun 19, 2024, 6:13 PM +03:00

--- players/1.sql
+++ players/1.sql
@@ -1,3 +1,6 @@
 
 -- In 1.sql, write a SQL query to find the hometown (including city, state, and country) of Jackie Robinson.
-SELECT 
+SELECT
+    birth_city, birth_state, birth_country
+FROM players
+WHERE first_name = 'Jackie' AND last_name = 'Robinson';
