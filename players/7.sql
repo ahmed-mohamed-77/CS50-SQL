718a92333132ac5f5ac9c2f8bdbce7beb4c98efc
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 18:01:11 +0000

    Wed, Jun 19, 2024, 9:01 PM +03:00

--- players/7.sql
+++ players/7.sql
@@ -1,4 +1,2 @@
-In 7.sql, write a SQL query to count the number of players
-who bat (or batted) right-handed and throw (or threw) left-handed, or vice versa.
-
-
+SELECT COUNT(*) FROM players
+WHERE (bats = 'R' AND throws = 'L') OR (bats = 'L' AND throws = 'R');
---
+++ players/8.sql
@@ -0,0 +1,3 @@
+Executing 8.sql results in a table with 2 columns and 1 row.
+
+SELECT
