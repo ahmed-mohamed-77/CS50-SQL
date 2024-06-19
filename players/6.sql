bb03f37289fbd45936349ee26de201c9968ede3b
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 15:40:34 +0000

    Wed, Jun 19, 2024, 6:40 PM +03:00

--- players/6.sql
+++ players/6.sql
@@ -1,4 +1,4 @@
 In 6.sql, write a SQL query to return the first name, last name, and debut date of players born in Pittsburgh, Pennsylvania (PA).
 Sort the results first by debut date—from most recent to oldest—then alphabetically by first name, followed by last name.
 
-SELECT first_name, last_name,
+SELECT first_name, last_name, debut
