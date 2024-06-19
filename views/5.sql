8160aa62cf295fc86ede26201a823e6f9eafce8a
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 19:48:22 +0000

    Wed, Jun 19, 2024, 10:48 PM +03:00

--- views/5.sql
+++ views/5.sql
@@ -1,7 +1,2 @@
-In 5.sql, write a SQL query to find the highest contrast value of prints by Hokusai.
-Name the column “Maximum Contrast”. Does Hokusai’s prints most contrasting print
-actually have much contrast?
-
-
-SELECT COUNT(*) FROM views
-WHERE artist = 'Hiroshige' AND english_title LIKE '%Eastern Capital%';
+SELECT MAX(contrast) FROM views
+WHERE artist = 'Hokusai' ;
