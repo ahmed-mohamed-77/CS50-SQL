c9160395efabc0a7e6e7c1c366a4329fe936288a
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 19:37:37 +0000

    Wed, Jun 19, 2024, 10:37 PM +03:00

--- views/3.sql
+++ views/3.sql
@@ -5,5 +5,5 @@ make it into the title?
 SELECT COUNT(*) FROM views
 WHERE english_title LIKE '%Fuji%'
 
-SELECT english_title FROM views
-WHERE english_title LIKE '%Fuji%';
+SELECT COUNT(*) FROM views
+WHERE english_title LIKE '%Fuji%' AND artist ='Hokusai';
