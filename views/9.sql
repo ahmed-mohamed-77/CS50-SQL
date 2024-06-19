23a00c54205bfb05ba3551dd0d61c086572f952b
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 20:04:17 +0000

    Wed, Jun 19, 2024, 11:04 PM +03:00

--- views/9.sql
+++ views/9.sql
@@ -1,5 +1,3 @@
-In 9.sql, write a SQL query to find the English title and artist of the print with
-the highest brightness.
-
 SELECT english_title, artist FROM views
-ORDER BY contrast DESC;
+ORDER BY brightness DESC
+LIMIT 5;
