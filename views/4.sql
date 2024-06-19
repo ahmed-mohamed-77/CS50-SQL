84fff696f905642132d665611f945c01e5f298dc
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 19:41:59 +0000

    Wed, Jun 19, 2024, 10:41 PM +03:00

---
+++ views/4.sql
@@ -0,0 +1,6 @@
+In 4.sql, write a SQL query to count how many prints by
+Hiroshige have English titles that refer to the “Eastern Capital”.
+Hiroshige’s prints were created in Japan’s “Edo period,”
+referencing the eastern capital city of Edo, now Tokyo.
+
+
