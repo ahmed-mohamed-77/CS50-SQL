2465fb3681e5309e8c1220b616e8afdb7fe822ab
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 18:17:20 +0000

    Wed, Jun 19, 2024, 9:17 PM +03:00

--- players/9.sql
+++ players/9.sql
@@ -1,7 +1,3 @@
-In 9.sql, write a SQL query to find the players who played their final game in the MLB in 2022.
-Sort the results alphabetically by first name, then by last name
-
-
 SELECT first_name, last_name FROM players
-WHERE STRFTIME('%Y', final_game) = 2022
+WHERE STRFTIME('%Y', final_game) = '2022'
 ORDER BY first_name ASC, last_name ASC;
