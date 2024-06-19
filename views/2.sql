392379be1e3c78ddddef9843257a3d5fd951362e
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 19:32:11 +0000

    Wed, Jun 19, 2024, 10:32 PM +03:00

--- views/2.sql
+++ views/2.sql
@@ -1,4 +1,2 @@
-In 2.sql, write a SQL query to list the average colors of prints by Hokusai that include “river” in
-the English title. (As an aside, do they have any hint of blue?)
-
-SELECT 
+SELECT average_color FROM views
+WHERE artist = 'Hokusai' AND english_title LIKE '%river%';
