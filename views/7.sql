8fb9993f8b0078c5e29ca4e8af4c5e3a934bb546
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 19:56:23 +0000

    Wed, Jun 19, 2024, 10:56 PM +03:00

---
+++ views/7.sql
@@ -0,0 +1,5 @@
+In 7.sql, write a SQL query to list the English titles of the 5 brightest prints by
+Hiroshige, from most to least bright. Compare them to this list on Wikipedia to see
+if your results match the print’s aesthetics.
+Executing 7.sql results in a table with 1 column and 5 rows
+
