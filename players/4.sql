94f8dc60f13cb804e19afbe53f0a6fbbe0fd9b1a
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 15:32:15 +0000

    Wed, Jun 19, 2024, 6:32 PM +03:00

--- players/4.sql
+++ players/4.sql
@@ -2,5 +2,5 @@ In 4.sql, write a SQL query to find the first and last names of players who were
 in the United States. Sort the results alphabetically by first name, then by last name.
 
 SELECT first_name, last_name FROM players
-WHERE birth_country = 'USA'
+WHERE birth_country NOT IN 'USA'
 ORDER BY first_name ASC, last_name ASC;
