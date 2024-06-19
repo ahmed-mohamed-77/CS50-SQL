f891922f9ca3efaffe74f745c333633d1c32d603
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 20:01:09 +0000

    Wed, Jun 19, 2024, 11:01 PM +03:00

--- views/7.sql
+++ views/7.sql
@@ -1,8 +1,4 @@
-In 7.sql, write a SQL query to list the English titles of the 5 brightest prints by
-Hiroshige, from most to least bright. Compare them to this list on Wikipedia to see
-if your results match the print’s aesthetics.
-Executing 7.sql results in a table with 1 column and 5 rows
-
-SELECT english_title FROM
+SELECT english_title FROM views
 WHERE artist = 'Hiroshige'
-ORDER BY 
+ORDER BY brightness DESC
+LIMIT 5;
