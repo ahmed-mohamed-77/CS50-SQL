73116546040cfda09985917a52e672f2a26a0e38
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 18:22:14 +0000

    Wed, Jun 19, 2024, 9:22 PM +03:00

--- players/6.sql
+++ players/6.sql
@@ -1,3 +1,4 @@
-SELECT first_name, last_name, debut FROM players
+SELECT first_name, last_name, debut
+FROM players
 WHERE birth_state = 'PA' AND birth_city = 'Pittsburgh'
-ORDER BY debut ASC, first_name ASC, last_name ASC ;
+ORDER BY debut DESC, first_name, last_name;
