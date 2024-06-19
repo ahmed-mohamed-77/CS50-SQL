f2d3dae9692671b5702a02bce8bef8fe16e9048e
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 18:11:28 +0000

    Wed, Jun 19, 2024, 9:11 PM +03:00

--- players/8.sql
+++ players/8.sql
@@ -1,8 +1,5 @@
-In 8.sql, write a SQL query to find the average height and weight,
-rounded to two decimal places, of baseball players who debuted on or after
-January 1st, 2000. Return the columns with the name “Average Height” and
-“Average Weight”, respectively.
-
-
-SELECT ROUND(AVG(height), 2), ROUND(AVG(weight), 2) FROM players
-WHERE debut >= '2000-01-01';
+SELECT
+    ROUND(AVG(height), 2) AS "Average Height",
+    ROUND(AVG(weight), 2) AS "Average Weight"
+FROM players
+    WHERE debut >= '2000-01-01';
---
+++ players/9.sql
@@ -0,0 +1,5 @@
+In 9.sql, write a SQL query to find the players who played their final game in the MLB in 2022.
+Sort the results alphabetically by first name, then by last name
+
+
+SELECT first_name, last_name
