8ee6dafe435938ad93f3b643cb4d391b9081649c
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 15:30:13 +0000

    Wed, Jun 19, 2024, 6:30 PM +03:00

--- players/3.sql
+++ players/3.sql
@@ -1,3 +1,1 @@
-In 3.sql, write a SQL query to find the ids of rows for which a value in the column debut is missing.
-
-SELECT 
+SELECT id FROM players WHERE debut IS NULL;
---
+++ players/4.sql
@@ -0,0 +1,5 @@
+In 4.sql, write a SQL query to find the first and last names of players who were not born
+in the United States. Sort the results alphabetically by first name, then by last name.
+
+SELECT first_name, last_name FROM players
+WHERE 
