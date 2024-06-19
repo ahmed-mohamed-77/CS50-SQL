553aace008ed55da7d035a0d39d65ef3d7878ac9
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 18:04:09 +0000

    Wed, Jun 19, 2024, 9:04 PM +03:00

--- players/8.sql
+++ players/8.sql
@@ -1,3 +1,7 @@
-Executing 8.sql results in a table with 2 columns and 1 row.
+In 8.sql, write a SQL query to find the average height and weight,
+rounded to two decimal places, of baseball players who debuted on or after
+January 1st, 2000. Return the columns with the name “Average Height” and
+“Average Weight”, respectively.
 
-SELECT
+
+SELECT ROUND(AVG(height), 2), ROUND(AVG(weight), 2)
