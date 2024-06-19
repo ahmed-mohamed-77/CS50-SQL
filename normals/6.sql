d9eb76ae765c00acfa24da548795904850b2bb79
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 21:17:27 +0000

    Thu, Jun 20, 2024, 12:17 AM +03:00

--- normals/6.sql
+++ normals/6.sql
@@ -1,12 +1,4 @@
-In 6.sql, write a SQL query to return all normal ocean temperatures at 50m of depth, as well as their respective degrees of latitude and longitude, within the Arabian Sea. Include latitude, longitude, and temperature columns. For simplicity, assume the Arabian Sea is encased in the following four coordinates:
-20° of latitude, 55° of longitude
-20° of latitude, 75° of longitude
-0° of latitude, 55° degrees of longitude
-0° of latitude, 75° degrees of longitude
-
-SELECT
-    (SELECT "50m" FROM Normals WHERE latitude = 20 AND longitude = 55),
-    (SELECT "50m" FROM Normals WHERE latitude = 20 AND longitude = 75),
-    (SELECT "50m" FROM Normals WHERE latitude = 0 AND longitude = 55),
-    (SELECT "50m" FROM Normals WHERE latitude = 0 AND longitude = 75)
-FROM Normals;
+SELECT latitude, longitude, "50m" AS temperature
+FROM Normals
+WHERE latitude BETWEEN 0 AND 20
+  AND longitude BETWEEN 55 AND 75;
