e8d8330c49fe355b33365e10efbc5562177823cf
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-19 19:38:14 +0000

    Wed, Jun 19, 2024, 10:38 PM +03:00

--- views/3.sql
+++ views/3.sql
@@ -1,9 +1,2 @@
-In 3.sql, write a SQL query to count how many prints by Hokusai include “Fuji” in the
-English title. Though all of Hokusai’s prints focused on Mt. Fuji, in how many did “Fuji”
-make it into the title?
-
-SELECT COUNT(*) FROM views
-WHERE english_title LIKE '%Fuji%'
-
 SELECT COUNT(*) FROM views
 WHERE english_title LIKE '%Fuji%' AND artist ='Hokusai';
