e472f6dda1fd56e910c5aacd1308c0246080207e
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-20 07:21:03 +0000

    Thu, Jun 20, 2024, 10:21 AM +03:00

--- moneyball/11.sql
+++ moneyball/11.sql
@@ -4,4 +4,7 @@ SELECT
     ROUND(S.salary/PF.H, 2) AS "dollars per hit"
 FROM players AS P
 JOIN salaries AS S ON P.id = S.player_id
-JOIN performances AS PF ON P.id = PF.player_id
+JOIN performances AS PF ON P.id = PF.player_id AND S.year = PF.year
+WHERE PF.year = 2001 AND "dollars per hit" IS NOT NULL
+ORDER BY "dollars per hit" ASC, P.first_name ASC, P.last_name ASC
+LIMIT 10;
