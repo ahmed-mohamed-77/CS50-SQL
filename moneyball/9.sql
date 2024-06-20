91f7c07746623189872e8c60096730bfe67724a3
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-20 07:03:26 +0000

    Thu, Jun 20, 2024, 10:03 AM +03:00

--- moneyball/9.sql
+++ moneyball/9.sql
@@ -1,1 +1,10 @@
-SELECT 
+SELECT
+    T.name,
+    ROUND(AVG(S.salary), 2 )AS "average salary"
+FROM salaries AS S
+JOIN teams AS T ON S.team_id = T.id
+GROUP BY T.name
+ORDER BY "average salary" ASC
+LIMIT 5;
+
+
