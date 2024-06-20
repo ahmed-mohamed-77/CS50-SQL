d392be4ac241879a599752a6b25bef870562864a
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-20 10:22:22 +0000

    Thu, Jun 20, 2024, 1:22 PM +03:00

--- moneyball/8.sql
+++ moneyball/8.sql
@@ -3,7 +3,6 @@ JOIN teams AS T ON S.team_id = T.id
 JOIN performances AS P ON T.id = P.team_id
 WHERE "H" = (
     SELECT MAX(H) FROM performances
-    WHERE "year" = 2001
-    )
+    ) AND "year" = 2001
 ORDER BY S.salary DESC
 LIMIT 1;
