df5f08352263d86ed0c0ee8317adb3a9098f1d5b
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-20 07:22:39 +0000

    Thu, Jun 20, 2024, 10:22 AM +03:00

--- moneyball/12.sql
+++ moneyball/12.sql
@@ -1,10 +1,10 @@
 SELECT
     P.first_name,
     P.last_name,
-    ROUND(S.salary/PF.H, 2) AS "dollars per hit"
 FROM players AS P
 JOIN salaries AS S ON P.id = S.player_id
 JOIN performances AS PF ON P.id = PF.player_id AND S.year = PF.year
+
 WHERE PF.year = 2001 AND "dollars per hit" IS NOT NULL
 ORDER BY "dollars per hit" ASC, P.first_name ASC, P.last_name ASC
 LIMIT 10;
