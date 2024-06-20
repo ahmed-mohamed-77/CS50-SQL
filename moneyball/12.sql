3103f98ad42e5d51a4bc4dc836facc11dfc0a656
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-20 10:53:13 +0000

    Thu, Jun 20, 2024, 1:53 PM +03:00

--- moneyball/12.sql
+++ moneyball/12.sql
@@ -1,16 +1,35 @@
-SELECT
-    P.first_name,
-    P.last_name
-FROM players AS P
-JOIN salaries AS S ON P.id = S.player_id
-JOIN performances AS PF ON P.id = PF.player_id AND S.year = PF.year
-
-WHERE "RBI" = (
-    SELECT ROUND(S.salary / PF.RBI, 2
-FROM players AS P
-JOIN salaries AS S ON P.id = S.player_id
-JOIN performances AS PF ON P.id = PF.player_id AND S.year = PF.year
-WHERE PF.year = 2001
+WITH PlayerCostPerHit AS (
+    SELECT
+        P.id,
+        P.first_name,
+        P.last_name,
+        (S.salary * 1.0 / PF.H) AS cost_per_hit
+    FROM players AS P
+    JOIN salaries AS S ON P.id = S.player_id
+    JOIN performances AS PF ON P.id = PF.player_id
+    WHERE S.year = 2001 AND PF.year = 2001
+    ORDER BY cost_per_hit ASC
+    LIMIT 10
+),
+PlayerCostPerRBI AS (
+    SELECT
+        P.id,
+        P.first_name,
+        P.last_name,
+        (S.salary * 1.0 / PF.RBI) AS cost_per_rbi
+    FROM players AS P
+    JOIN salaries AS S ON P.id = S.player_id
+    JOIN performances AS PF ON P.id = PF.player_id
+    WHERE S.year = 2001 AND PF.year = 2001
+    ORDER BY cost_per_rbi ASC
+    LIMIT 10
 )
-ORDER BY P.id ASC, P.last_name ASC
-LIMIT 10;
+SELECT DISTINCT
+    first_name,
+    last_name
+FROM (
+    SELECT first_name, last_name FROM PlayerCostPerHit
+    UNION
+    SELECT first_name, last_name FROM PlayerCostPerRBI
+)
+ORDER BY last_name;
