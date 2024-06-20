4c48773ef9c63e573677ab343720da7236a3e679
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-20 05:58:59 +0000

    Thu, Jun 20, 2024, 8:58 AM +03:00

--- moneyball/4.sql
+++ moneyball/4.sql
@@ -1,8 +1,9 @@
 SELECT
     P.first_name, P.last_name, S.salary
-FROM performances AS P
+FROM players AS P
 JOIN salaries AS S ON P.id = S.player_id
 WHERE S.year = 2001
-ORDER BY S.salary ASC, P.first_name ASC, P.last_name ASC, S.player_id ASC;
+ORDER BY S.salary ASC, P.first_name ASC, P.last_name ASC, S.player_id ASC
+LIMIT 50;
 
 
