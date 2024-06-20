0956ba1cc3211f796ac78aaa1cc74d759a2e5710
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-20 06:47:09 +0000

    Thu, Jun 20, 2024, 9:47 AM +03:00

--- moneyball/8.sql
+++ moneyball/8.sql
@@ -1,4 +1,10 @@
 SELECT salary FROM salaries
 WHERE id = (
-    SELECT 
-)
+    SELECT id FROM teams
+        WHERE team_id IN (
+            SELECT id FROM performances
+                WHERE H = (
+                    SELECT MAX(H) FROM performances
+                )
+        )
+);
