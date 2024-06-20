a52858a73085c7f3f1e2adc27e345080d1931c54
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-20 06:30:08 +0000

    Thu, Jun 20, 2024, 9:30 AM +03:00

--- moneyball/5.sql
+++ moneyball/5.sql
@@ -1,29 +1,4 @@
 SELECT T.name FROM teams AS T
-JOIN salaries AS S ON T.id = S.team_id
-JOIN players AS P ON S.player_id = P.id
-WHERE P.id = 14190;
-
-
-SELECT * FROM teams AS T
-JOIN salaries AS S ON T.id = S.team_id
-JOIN players AS P ON S.player_id = P.id;
-
-SELECT first_name , last_name FROM players
-WHERE first_name = 'Satchel' AND last_name = 'Paige';
-
-SELECT P.first_name , P.last_name FROM teams AS T
-JOIN salaries AS S ON T.id = S.team_id
-JOIN players AS P ON S.player_id = P.id
-WHERE P.first_name = 'Satchel' ;
-
-
-SELECT * FROM salaries
-WHERE player_id IN (SELECT id FROM players WHERE first_name = 'Satchel' AND last_name = 'Paige');
-
-SELECT * FROM salaries
-WHERE player_id = (SELECT id FROM players WHERE first_name = 'Satchel' AND last_name = 'Paige');
-
-SELECT * FROM salaries AS S
-FULL OUTER JOIN players AS P ON S.player_id = P.id
+JOIN performances AS PF ON T.id = PF.team_id
+JOIN players AS P ON PF.player_id = P.id
 WHERE P.first_name = 'Satchel' AND P.last_name = 'Paige';
-
