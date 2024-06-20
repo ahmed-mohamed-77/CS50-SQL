35c1d23e1dc5337a5b8ae3cb0301ea9d9ea287c8
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-20 10:50:45 +0000

    Thu, Jun 20, 2024, 1:50 PM +03:00

--- moneyball/6.sql
+++ moneyball/6.sql
@@ -1,6 +1,7 @@
-SELECT T.name, P.H AS "total hits" FROM teams AS T
+SELECT T.name, COUNT(P.H) AS "total hits" FROM teams AS T
 JOIN performances AS P ON T.id = P.team_id
 WHERE P.year = 2001
+GROUP BY T.name
 ORDER BY P.H DESC
 LIMIT 5;
 
