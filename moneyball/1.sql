099325011e51e23dfb3ae09fc2385c6a9d746c31
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-20 05:18:17 +0000

    Thu, Jun 20, 2024, 8:18 AM +03:00

--- moneyball/1.sql
+++ moneyball/1.sql
@@ -1,3 +1,6 @@
-SELECT AVG("salary") , "year" FROM salaries
+SELECT
+    "year",
+    ROUND(AVG("salary"), 2)AS "average salary"
+FROM salaries
 GROUP BY "year"
-ORDER BY "year;
+ORDER BY "year" DESC;
