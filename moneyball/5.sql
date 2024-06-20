ee74c23bb8d2297ffe794e4ae973d83d68ddc5ac
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-20 06:07:54 +0000

    Thu, Jun 20, 2024, 9:07 AM +03:00

--- moneyball/5.sql
+++ moneyball/5.sql
@@ -1,4 +1,10 @@
-SELECT "name" FROM teams
-WHERE 
+SELECT T.name FROM teams AS T
+JOIN salaries AS S ON T.id = S.team_id
+JOIN players AS P ON S.player_id = P.id;
+WHERE P.first_name = 'Satchel' AND P.last_name = 'Paige';
 
-WHERE "first_name" = 'Satchel' AND "last_name" = 'Paige';
+
+SELECT * FROM teams AS T
+JOIN salaries AS S ON T.id = S.team_id
+JOIN players AS P ON S.player_id = P.id;
+
