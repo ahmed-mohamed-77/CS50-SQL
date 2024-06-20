b64bfed0355b8ece503b870454eb2acf5dc27e4b
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-20 06:51:49 +0000

    Thu, Jun 20, 2024, 9:51 AM +03:00

--- moneyball/8.sql
+++ moneyball/8.sql
@@ -1,10 +1,11 @@
-SELECT salary FROM salaries
-WHERE id = (
-    SELECT id FROM teams
-        WHERE id IN (
-            SELECT id FROM performances
-                WHERE H = (
-                    SELECT MAX(H) FROM performances
-                )
-        )
+SELECT S.salary FROM salaries AS S
+JOIN teams AS T ON S.team_id = T.id
+JOIN performances AS P ON T.id = P.team_id
+WHERE P.year = 2001 AND P.H = (
+    SELECT MAX(H) FROM performances
+);
+
+SELECT * FROM performances
+WHERE year = 2001 AND H = (
+    SELECT MAX(H) FROM performances
 );
