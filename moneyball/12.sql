e9e54bcb2359e1653ea7faaac13c6b5799f1e2da
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-20 07:25:53 +0000

    Thu, Jun 20, 2024, 10:25 AM +03:00

--- moneyball/12.sql
+++ moneyball/12.sql
@@ -5,8 +5,12 @@ FROM players AS P
 JOIN salaries AS S ON P.id = S.player_id
 JOIN performances AS PF ON P.id = PF.player_id AND S.year = PF.year
 
-WHERE PF.year = 2001 AND "RBI" = (
-    SELECT ROUND()
+WHERE "RBI" = (
+    SELECT ROUND(S.salary / PF.RBI, 2)
+FROM players AS P
+JOIN salaries AS S ON P.id = S.player_id
+JOIN performances AS PF ON P.id = PF.player_id AND S.year = PF.year
+WHERE PF.year = 2001
 )
-ORDER BY "dollars per hit" ASC, P.first_name ASC, P.last_name ASC
+ORDER BY P.id ASC, P.first_name ASC, P.last_name ASC
 LIMIT 10;
