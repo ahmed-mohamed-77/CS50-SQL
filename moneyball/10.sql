a29196f594420b13b1680b81c272cb92dbaf1d1f
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-20 07:11:43 +0000

    Thu, Jun 20, 2024, 10:11 AM +03:00

--- moneyball/10.sql
+++ moneyball/10.sql
@@ -1,5 +1,10 @@
-SELECT P.first_name, P.last_name, S.salary, PF.H
+SELECT
+    P.first_name,
+    P.last_name,
+    S.salary,
+    PF.year,
+    PF.HR
 FROM players AS P
 JOIN salaries AS S ON P.id = S.player_id
-JOIN performances AS PF ON P.id = PF.player_id
-WHERE PF.H 
+JOIN performances AS PF ON P.id = PF.player_id AND S.year = PF.year
+
