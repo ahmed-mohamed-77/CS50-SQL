7b85c0581973e364274113bf2fb4fcc9f1a62929
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-20 06:20:51 +0000

    Thu, Jun 20, 2024, 9:20 AM +03:00

--- moneyball/5.sql
+++ moneyball/5.sql
@@ -18,5 +18,5 @@ WHERE P.first_name = 'Satchel' ;
 
 
 SELECT * FROM salaries
-   ...> WHERE player_id IN (SELECT id FROM players WHERE first_name = 'Satchel' AND last_name = 'Paige');
-sqlite>
+WHERE player_id IN (SELECT id FROM players WHERE first_name = 'Satchel' AND last_name = 'Paige');
+
