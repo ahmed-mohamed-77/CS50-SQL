a36beb91e54d7a5ba2ea88335c9746dadf968880
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-21 04:22:54 +0000

    Fri, Jun 21, 2024, 7:22 AM +03:00

--- dese/4.sql
+++ dese/4.sql
@@ -1,9 +1,5 @@
-rite a SQL query to find the 10 cities with the most public schools.
-Your query should return the names of the cities and the number of public schools within them
-
-
-SELECT "city" , SUM("type") FROM schools
+SELECT "city" , COUNT("type") FROM schools
 WHERE "type" = 'Public School'
 GROUP BY "city"
-ORDER BY SUM("type") DESC, "city" ASC
+ORDER BY COUNT("type") DESC, "city" ASC
 LIMIT 10;
