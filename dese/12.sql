138af02805ce81fe705c45146651c90a4edc3cb0
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-21 05:19:30 +0000

    Fri, Jun 21, 2024, 8:19 AM +03:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,6 +1,5 @@
-SELECT S.name, SUM(E.per_pupil_expenditure), GR.graduated FROM schools AS S
-JOIN graduation_rates AS GR ON S.id = GR.school_id
-JOIN districts AS D ON S.district_id = D.id
+SELECT D.name, SUM(E.per_pupil_expenditure), SE.exemplary FROM districts AS D
 JOIN expenditures AS E ON D.id = E.district_id
-GROUP BY S.name
-ORDER BY SUM(E.per_pupil_expenditure) DESC, S.name ASC;
+JOIN staff_evaluations AS SE ON D.id = SE.district_id
+WHERE 
+GROUP BY D.name
