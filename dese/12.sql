b13b72d1352879d53c17eb99190fa6979ad5c85c
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-21 05:13:04 +0000

    Fri, Jun 21, 2024, 8:13 AM +03:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,2 +1,7 @@
-SELECT "name" FROM districts
-WHERE 
+SELECT "name" FROM "districts"
+WHERE "id" > (
+    SELECT "district_id" FROM "expenditures"
+    WHERE "per_pupil_expenditure" = (
+        SELECT AVG("per_pupil_expenditure") FROM "expenditures"
+    )
+)
