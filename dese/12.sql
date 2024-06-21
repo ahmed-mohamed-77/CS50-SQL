0ddaf2cdeed7e87312b338c7d98c3b1c116b2e31
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-21 05:24:57 +0000

    Fri, Jun 21, 2024, 8:24 AM +03:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,10 +1,14 @@
-SELECT D.name, SUM(E.per_pupil_expenditure), SE.exemplary FROM districts AS D
+SELECT D.name,
+       SUM(E.per_pupil_expenditure) AS per_pupil_expenditure,
+       AVG(SE.exemplary) AS percentage_exemplary
+FROM districts AS D
 JOIN expenditures AS E ON D.id = E.district_id
 JOIN staff_evaluations AS SE ON D.id = SE.district_id
 WHERE E.per_pupil_expenditure > (
-    SELECT AVG("per_pupil_expenditure") FROM expenditures
-) AND SE.exemplary > (
-    SELECT AVG("exemplary") FROM staff_evaluations
+    SELECT AVG(per_pupil_expenditure) FROM expenditures
+)
+AND SE.exemplary > (
+    SELECT AVG(exemplary) FROM staff_evaluations
 )
 GROUP BY D.name
-ORDER BY SE.exemplary DESC, SUM(E.per_pupil_expenditure) DESC;
+ORDER BY percentage_exemplary DESC, per_pupil_expenditure DESC;
