8524f910a67b495464c2f9ffe9f2f6083c83ef51
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-21 05:00:56 +0000

    Fri, Jun 21, 2024, 8:00 AM +03:00

--- dese/10.sql
+++ dese/10.sql
@@ -1,6 +1,7 @@
 SELECT D.name, SUM(E.per_pupil_expenditure)
 FROM districts AS D
 JOIN expenditures AS E ON D.id = E.district_id
+WHERE D.name LIKE '%Public School%'
 GROUP BY D.name
 ORDER BY SUM(E.per_pupil_expenditure) DESC
 LIMIT 10;
