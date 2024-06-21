3edcdb5357d613b736e5b9b9bb75d42ad486c1cb
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-21 04:10:26 +0000

    Fri, Jun 21, 2024, 7:10 AM +03:00

--- dese/1.sql
+++ dese/1.sql
@@ -1,9 +1,2 @@
-SELECT * FROM schools AS S
-JOIN districts AS D ON S.district_id = D.id;
-WHERE S.city LIKE 'Mass%' AND D.city LIKE 'Mass%';
-
-SELECT * FROM districts;
-
-SELECT * FROM schools WHERE state = (
-    SELECT DISTINCT state FROM schools
-);
+SELECT "name", "city" FROM schools
+WHERE "state" = 'MA' AND "type" = 'Public School';
