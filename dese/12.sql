0f2e7fda14fce1a5602c4fe1518b07a28d713d2a
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-21 05:15:43 +0000

    Fri, Jun 21, 2024, 8:15 AM +03:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,7 +1,6 @@
-SELECT "name" FROM "districts"
-WHERE "id" > (
-    SELECT "district_id" FROM "expenditures"
-    WHERE "per_pupil_expenditure" = (
-        SELECT AVG("per_pupil_expenditure") FROM "expenditures"
-    )
-)
+SELECT S.name, SUM(E.per_pupil_expenditure), GR.graduated FROM schools AS S
+JOIN graduation_rates AS GR ON S.id = GR.school_id
+JOIN districts AS D ON S.district_id = D.id
+JOIN expenditures AS E ON D.id = E.district_id
+GROUP BY S.name
+ORDER BY SUM(E.per_pupil_expenditure) DESC, S.name ASC;
