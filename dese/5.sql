e1a48056b55de5ac573396b0f38fec7f09fa7066
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-21 04:27:51 +0000

    Fri, Jun 21, 2024, 7:27 AM +03:00

--- dese/5.sql
+++ dese/5.sql
@@ -1,10 +1,5 @@
-DESE would like you to determine in what cities additional public schools might be needed.
-
-In 5.sql, write a SQL query to find cities with 3 or fewer public schools. Your query should return
-the names of the cities and the number of public schools within them, ordered from greatest number of
-public schools to least. If two cities have the same number of public schools, order them alphabetically.
-
 SELECT "city" , COUNT("type") FROM schools
-WHERE "type" = 'Public School' AND COUNT("type") <= 3
+WHERE "type" = 'Public School'
 GROUP BY "city"
+HAVING COUNT("type") <= 3
 ORDER BY COUNT("type") DESC, "city" ASC;
