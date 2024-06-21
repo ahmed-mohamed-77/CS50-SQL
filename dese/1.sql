d94cbaf1fdd47edc09c58ae76299c5f8148c2c87
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-21 04:07:07 +0000

    Fri, Jun 21, 2024, 7:07 AM +03:00

--- dese/1.sql
+++ dese/1.sql
@@ -3,3 +3,7 @@ JOIN districts AS D ON S.district_id = D.id;
 WHERE S.city LIKE 'Mass%' AND D.city LIKE 'Mass%';
 
 SELECT * FROM districts;
+
+SELECT * FROM schools WHERE state = (
+    SELECT DISTINCT state FROM schools
+);
