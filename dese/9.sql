70ab4ed03a12510af7e23a4e5a5a0d10e1519464
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-21 04:52:21 +0000

    Fri, Jun 21, 2024, 7:52 AM +03:00

--- dese/9.sql
+++ dese/9.sql
@@ -3,6 +3,6 @@ In 9.sql, write a SQL query to find the name (or names) of the school district(s
  with the single least number of pupils. Report only the name(s).
 
 
-SELECT D.name, MIN(SUM(E.pupils)) FROM districts AS D
+SELECT D.name, SUM(E.pupils) FROM districts AS D
 JOIN expenditures AS E ON D.id = E.district_id
 GROUP BY D.name;
