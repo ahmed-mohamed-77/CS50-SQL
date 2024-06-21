720d7bcb074879549eb55346e8e74a9271642ff3
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-21 03:53:25 +0000

    Fri, Jun 21, 2024, 6:53 AM +03:00

--- dese/1.sql
+++ dese/1.sql
@@ -1,3 +1,6 @@
 SELECT "name", "city" FROM schools
-WHERE "type" = 'Public School' AND "city" =
-Massachusetts
+WHERE "type" = 'Public School' AND "city" = 'Massachusetts';
+
+SELECT "name", "city" FROM schools
+WHERE  "city" = 'Massachusetts';
+
