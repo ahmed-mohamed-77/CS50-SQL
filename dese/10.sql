8608f9115113fbbff497820a6408768e49ef742a
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-21 05:04:36 +0000

    Fri, Jun 21, 2024, 8:04 AM +03:00

--- dese/10.sql
+++ dese/10.sql
@@ -1,7 +1,7 @@
 SELECT D.name, SUM(E.per_pupil_expenditure)
 FROM districts AS D
 JOIN expenditures AS E ON D.id = E.district_id
-WHERE D.name LIKE '%Public School%'
+WHERE D.type ='Public School District'
 GROUP BY D.name
 ORDER BY SUM(E.per_pupil_expenditure) DESC
 LIMIT 10;
--- dese/11.sql
+++ dese/11.sql
@@ -1,1 +1,4 @@
-SELECT FROM s
+SELECT * FROM schools AS S
+JOIN graduation_rates AS GR ON S.id = GR.school_id
+JOIN districts AS D ON S.district_id = D.id
+JOIN expenditures AS E ON D.id = E.district_id
