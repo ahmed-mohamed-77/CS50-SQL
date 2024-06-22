fc448015288e28a01c11da6f4b616f1f343df658
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-22 13:58:03 +0000

    Sat, Jun 22, 2024, 4:58 PM +03:00

--- packages/log.sql
+++ packages/log.sql
@@ -1,40 +1,10 @@
 
 -- *** The Lost Letter ***
-
--- *** The Devious Delivery ***
-
--- *** The Forgotten Gift ***
-
-SELECT *  FROM addresses
-WHERE "address" = '109 Tileston Street';
-
-SELECT *  FROM addresses
-WHERE "address" = '728 Maple Place';
-
 SELECT *  FROM addresses
 WHERE "address" = '900 Somerville Avenue';
 
 SELECT *  FROM addresses
 WHERE "address" = '2 Finnigan Street';
-
-SELECT * FROM "packages"
-WHERE "from_address_id" = (
-    SELECT "id"  FROM addresses
-    WHERE "address" = '109 Tileston Street'
-);
-
-SELECT * FROM "packages"
-WHERE "from_address_id" = 9873;
-
-
-SELECT * FROM "scans"
-WHERE "address_id" = (
-    SELECT "id"  FROM addresses
-    WHERE "address" = '109 Tileston Street'
-);
-SELECT * FROM "scans"
-WHERE "address_id" =9873;
-
 
 SELECT * FROM "scans"
 WHERE "address_id" = (
@@ -54,8 +24,36 @@ WHERE P.from_address_id = (
 
 
 SELECT * FROM "packages" WHERE id = 384;
-SELECT * FROM "packages" WHERE id = 2437;
-SELECT * FROM "packages" WHERE id = 3529;
-
-
 SELECT * FROM "scans" WHERE "package_id" = 384;
+
+-- *** The Devious Delivery ***
+SELECT *  FROM addresses
+WHERE "address" = '109 Tileston Street';
+
+SELECT *  FROM addresses
+WHERE "address" = '728 Maple Place';
+
+
+
+SELECT * FROM "packages"
+WHERE "from_address_id" = (
+    SELECT "id"  FROM addresses
+    WHERE "address" = '109 Tileston Street'
+);
+
+SELECT * FROM "packages"
+WHERE "from_address_id" = 9873;
+
+
+SELECT * FROM "scans"
+WHERE "address_id" = (
+    SELECT "id"  FROM addresses
+    WHERE "address" = '109 Tileston Street'
+);
+SELECT * FROM "scans"
+WHERE "address_id" =9873;
+
+-- *** The Forgotten Gift ***
+
+
+
