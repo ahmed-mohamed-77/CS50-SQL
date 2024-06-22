b5074894fa4c8723ed6ce2b3374fb065acf7e7e0
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-22 13:24:43 +0000

    Sat, Jun 22, 2024, 4:24 PM +03:00

--- packages/log.sql
+++ packages/log.sql
@@ -10,3 +10,9 @@ WHERE "address" = '109 Tileston Street';
 
 SELECT *  FROM addresses
 WHERE "address" = '728 Maple Place';
+
+SELECT * FROM "packages"
+WHERE "from_address_id" = (
+    SELECT "id"  FROM addresses
+    WHERE "address" = '109 Tileston Street'
+);
