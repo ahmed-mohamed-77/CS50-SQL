21b88da34821ee78bb524307cf509426e285a1b3
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-23 18:57:21 +0000

    Sun, Jun 23, 2024, 9:57 PM +03:00

--- alt/schema.sql
+++ alt/schema.sql
@@ -7,15 +7,6 @@ CREATE TABLE IF NOT EXISTS "passengers" (
     CONSTRAINT passenger_id PRIMARY KEY ("id")
 );
 
-
-Check-Ins
-When passengers arrive at ATL, they’ll often “check in” to their flights. That’s them telling us they’re here and all set to board.
-We’d like to keep a tidy log of such moments. And what would we need to log, you ask? Well, here’s what we need:
-
-The exact date and time at which our passenger checked in
-The flight they are checking in for, of course. Can’t lose track of where they’re headed, now can we?
-
-
 CREATE TABLE IF NOT EXISTS "check_ins" (
     "id" INTEGER,
     "passenger_id" INTEGER,
@@ -23,5 +14,21 @@ CREATE TABLE IF NOT EXISTS "check_ins" (
     "time" strftime("%I:%M:%S: %f", "time") NOT NULL,
 
     CONSTRAINT check_ins_pk PRIMARY KEY ("id"),
-    CONSTRAINT check_ins_fk FORIGEN KEY
+    CONSTRAINT check_ins_fk FOREIGN KEY ("passenger_id") REFERENCES "passengers"("id")
+);
+
+
+Airlines
+ATL’s a hub for many domestic and international airlines: names like Delta, British Airways, Air France, Korean Air, and Turkish Airlines. The list goes on.
+So here’s what we track:
+
+The name of the airline
+The “concourse” or, shall I say, the section of our airport where the airline operates. We have 7 concourses: A, B, C, D, E, F, and T.
+
+CREATE TABLE IF NOT EXISTS "airlines" (
+    "id" INTEGER,
+    "name" VARCHAR(50) NOT NULL,
+    "section" CHECK ("section" IN (A, B, C, D, E, F,T))
+
+    CONSTRAINT airlines_pk PRIMARY KEY ("id"),
 )
