2c8b7262f49bb52682539c408180854f03784fe5
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-23 18:45:57 +0000

    Sun, Jun 23, 2024, 9:45 PM +03:00

--- alt/schema.sql
+++ alt/schema.sql
@@ -1,1 +1,22 @@
+CREATE TABLE IF NOT EXISTS "passengers" (
+    "id" INTEGER,
+    "first_name" VARCHAR(50) NOT NULL,
+    "last_name" VARCHAR(50) NOT NULL,
+    "age" INTEGER NOT NULL,
 
+    CONSTRAINT passenger_id PRIMARY KEY ("id")
+);
+
+
+Check-Ins
+When passengers arrive at ATL, they’ll often “check in” to their flights. That’s them telling us they’re here and all set to board.
+We’d like to keep a tidy log of such moments. And what would we need to log, you ask? Well, here’s what we need:
+
+The exact date and time at which our passenger checked in
+The flight they are checking in for, of course. Can’t lose track of where they’re headed, now can we?
+
+
+CREATE TABLE IF NOT EXISTS "check_ins" (
+    "id" INTEGER,
+    "date"
+)
