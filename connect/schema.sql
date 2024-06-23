2c2dc74201f706a89684a4f3335a4f400f58b6ac
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-23 18:39:14 +0000

    Sun, Jun 23, 2024, 9:39 PM +03:00

--- connect/schema.sql
+++ connect/schema.sql
@@ -1,24 +1,62 @@
-CREATE TABLE IF NOT EXISTS "users" (
-    "id" INTEGER,
-    "user_name" VARCHAR(100) NOT NULL,
-    "password" VARCHAR(30) NOT NULL,
+CREATE TABLE users(
+    "id" INT,
+    "first_name" TEXT NOT NULL,
+    "Last_name" TEXT NOT NULL,
+    "username" TEXT NOT NULL UNIQUE,
+    "password" TEXT NOT NULL,
 
-    CONSTRAINT user_pk PRIMARY KEY ("id")
+    CONSTRAINT user_PK PRIMARY KEY ("id")
 );
 
-CREATE TABLE IF NOT EXISTS "school" (
-    "id" INTEGER,
-    "name" VARCHAR(50) NOT NULL,
-    "type" VARCHAR(50) NOT NULL,
-    "founded_year" VARCHAR(15) NOT NULL,
+CREATE TABLE schools(
+    "id" INT,
+    "name" TEXT NOT NULL UNIQUE,
+    "school_type" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "founded_year" INT NOT NULL,
 
-    CONSTRAINT school_pk PRIMARY KEY ("id")
+    CONSTRAINT school_PK PRIMARY KEY ("id")
 );
 
-CREATE TABLE IF NOT EXISTS "companies"(
-    "id" INTEGER,
-    "company_industry" VARCHAR(50) NOT NULL,
-    "company_location" VARCHAR(50) NOT NULL,
+CREATE TABLE companies(
+    "id" INT,
+    "name" TEXT NOT NULL,
+    "industry" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
 
-    CONSTRAINT companies_pk PRIMARY KEY ("id")
+    CONSTRAINT company_PK PRIMARY KEY("id")
+);
+
+CREATE TABLE users_connections(
+    "user1_id" INT,
+    "user2_id" INT,
+
+    CONSTRAINT users_connections_PK PRIMARY KEY ("user1_id", "user2_id"),
+    CONSTRAINT users_connections_users_FK FOREIGN KEY ("user1_id") REFERENCES "users"("id"),
+    CONSTRAINT users_connections_users_FK FOREIGN KEY ("user2_id") REFERENCES "users"("id")
+
+);
+
+CREATE TABLE schools_users(
+    "user_id" INT,
+    "school_id" INT,
+    "start_date" DATE NOT NULL,
+    "end_date" DATE NOT NULL,
+    "degree" TEXT NOT NULL,
+
+    CONSTRAINT schooluser_PK PRIMARY KEY ("user_id", "school_id"),
+    CONSTRAINT schooluser_user_PK FOREIGN KEY ("user_id") REFERENCES "users"("id"),
+    CONSTRAINT schooluser_school_PK FOREIGN KEY ("school_id") REFERENCES "schools"("id")
+);
+
+CREATE TABLE companies_users(
+    "user_id" INT,
+    "company_id" INT,
+    "start_date" DATE NOT NULL,
+    "end_date" DATE NOT NULL,
+    "company_title" TEXT NOT NULL,
+
+    CONSTRAINT companiesuser_PK PRIMARY KEY ("user_id", "company_id"),
+    CONSTRAINT companiesusers_users_FK FOREIGN KEY ("user_id") REFERENCES "users"("id"),
+    CONSTRAINT companiesusers_company_FK FOREIGN KEY ("company_id") REFERENCES "companies"("id")
 );
