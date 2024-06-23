61fc066d390a2113da3dffa2c3393b9b574c8ea4
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-23 18:36:54 +0000

    Sun, Jun 23, 2024, 9:36 PM +03:00

--- connect/schema.sql
+++ connect/schema.sql
@@ -6,4 +6,19 @@ CREATE TABLE IF NOT EXISTS "users" (
     CONSTRAINT user_pk PRIMARY KEY ("id")
 );
 
+CREATE TABLE IF NOT EXISTS "school" (
+    "id" INTEGER,
+    "name" VARCHAR(50) NOT NULL,
+    "type" VARCHAR(50) NOT NULL,
+    "founded_year" VARCHAR(15) NOT NULL,
 
+    CONSTRAINT school_pk PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "companies"(
+    "id" INTEGER,
+    "company_industry" VARCHAR(50) NOT NULL,
+    "company_location" VARCHAR(50) NOT NULL,
+
+    CONSTRAINT companies_pk PRIMARY KEY ("id")
+);
