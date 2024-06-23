91b2e4e761e46f43941454d391c4866162795e5e
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-23 05:53:38 +0000

    Sun, Jun 23, 2024, 8:53 AM +03:00

--- donuts/schema.sql
+++ donuts/schema.sql
@@ -9,19 +9,20 @@ CREATE TABLE IF NOT EXISTS "ingredients" (
 );
 
 
-Donuts
-We’ll need to include our selection of donuts, past and present! For each donut on the menu, we’d love to include three things:
+    Donuts
+    We’ll need to include our selection of donuts, past and present! For each donut on the menu, we’d love to include three things:
 
-The name of the donut
-Whether the donut is gluten-free
-The price per donut
-Oh, and it’s important that we be able to look up the ingredients for each of the donuts!
+    The name of the donut
+    Whether the donut is gluten-free
+    The price per donut
+    Oh, and it’s important that we be able to look up the ingredients for each of the donuts!
 
 
 CREATE TABLE IF NOT EXISTS "donuts" (
     "id" INTEGER,
     "name" VARCHAR(50) NOT NULL,
     "gluten_free" INTEGER NOT NULL CHECK ("gluten_free" IN (0,1)),
-    
+    "price" REAL NOT NULL,
+
     CONSTRAINT donuts_PK PRIMARY KEY ("id")
 )
