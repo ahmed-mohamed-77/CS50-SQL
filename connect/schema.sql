9d2778bf8f2fe5a31a6c946dbfdc43bdc86f0829
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-23 18:30:58 +0000

    Sun, Jun 23, 2024, 9:30 PM +03:00

--- connect/schema.sql
+++ connect/schema.sql
@@ -1,62 +1,8 @@
-CREATE TABLE users(
-    "id" INT,
-    "first_name" TEXT NOT NULL,
-    "Last_name" TEXT NOT NULL,
-    "username" TEXT NOT NULL UNIQUE,
-    "password" TEXT NOT NULL,
+Users
+The heart of LinkedIn’s platform is its people. Your database should be able to represent the following information about LinkedIn’s users:
 
-    CONSTRAINT user_PK PRIMARY KEY ("id")
-);
+Their first and last name
+Their username
+Their password
+Keep in mind that, if a company is following best practices, application passwords are “hashed.” No need to worry about hashing passwords here, though.
 
-CREATE TABLE schools(
-    "id" INT,
-    "name" TEXT NOT NULL UNIQUE,
-    "school_type" TEXT NOT NULL,
-    "location" TEXT NOT NULL,
-    "founded_year" INT NOT NULL,
-
-    CONSTRAINT school_PK PRIMARY KEY ("id")
-);
-
-CREATE TABLE companies(
-    "id" INT,
-    "name" TEXT NOT NULL,
-    "industry" TEXT NOT NULL,
-    "location" TEXT NOT NULL,
-
-    CONSTRAINT company_PK PRIMARY KEY("id")
-);
-
-CREATE TABLE users_connections(
-    "user1_id" INT,
-    "user2_id" INT,
-
-    CONSTRAINT users_connections_PK PRIMARY KEY ("user1_id", "user2_id"),
-    CONSTRAINT users_connections_users_FK FOREIGN KEY ("user1_id") REFERENCES "users"("id"),
-    CONSTRAINT users_connections_users_FK FOREIGN KEY ("user2_id") REFERENCES "users"("id")
-
-);
-
-CREATE TABLE schools_users(
-    "user_id" INT,
-    "school_id" INT,
-    "start_date" DATE NOT NULL,
-    "end_date" DATE NOT NULL,
-    "degree" TEXT NOT NULL,
-
-    CONSTRAINT schooluser_PK PRIMARY KEY ("user_id", "school_id"),
-    CONSTRAINT schooluser_user_PK FOREIGN KEY ("user_id") REFERENCES "users"("id"),
-    CONSTRAINT schooluser_school_PK FOREIGN KEY ("school_id") REFERENCES "schools"("id")
-);
-
-CREATE TABLE companies_users(
-    "user_id" INT,
-    "company_id" INT,
-    "start_date" DATE NOT NULL,
-    "end_date" DATE NOT NULL,
-    "company_title" TEXT NOT NULL,
-
-    CONSTRAINT companiesuser_PK PRIMARY KEY ("user_id", "company_id"),
-    CONSTRAINT companiesusers_users_FK FOREIGN KEY ("user_id") REFERENCES "users"("id"),
-    CONSTRAINT companiesusers_company_FK FOREIGN KEY ("company_id") REFERENCES "companies"("id")
-);
