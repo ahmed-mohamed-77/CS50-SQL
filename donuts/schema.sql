eb46d8334a35c57a883f19d7ccce6cea32d5cf82
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-23 05:34:50 +0000

    Sun, Jun 23, 2024, 8:34 AM +03:00

--- donuts/schema.sql
+++ donuts/schema.sql
@@ -1,1 +1,9 @@
-CREATE TABLE IF NOT EXISTS
+Ingredients
+We certainly need to keep track of our ingredients. Some of the typical ingredients we
+use include flour, yeast, oil, butter, and several different types of sugar. Moreover, we would love to keep track of
+the price we pay per unit of ingredient (whether it’s pounds, grams, etc.).
+
+CREATE TABLE IF NOT EXISTS "ingredients" (
+    "id" INTEGER NOT NULL,
+    
+)
