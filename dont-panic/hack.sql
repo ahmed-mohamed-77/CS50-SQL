62320bb32c9bb0f272c4f263920084a82c726840
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-24 03:34:51 +0000

    Mon, Jun 24, 2024, 6:34 AM +03:00

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -8,4 +8,4 @@ SET "new_password" = 'cb4772eeda0ae562119b6c4be2e0889a'
 WHERE "old_username" = 'admin' AND "new_username" = 'admin';
 
 DELETE FROM "user_logs"
-WHERE "old_username" != 'admin' AND "new_username" != 'admin';
+WHERE "id" <> 52;
