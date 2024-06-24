d6c7fa58e819a52cea9626b721c2f8851e47466c
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-24 20:26:22 +0000

    Mon, Jun 24, 2024, 11:26 PM +03:00

--- bnb/available.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW available AS
-SELECT L.id, L.property_type, L.host_name, A.date
-FROM listings AS L
-JOIN availabilities AS A ON L.id = A.listing_id
-WHERE A.available = "TRUE";
--- bnb/june_vacancies.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW june_vacancies AS
-SELECT
-L.id, L.property_type, L.host_name, COUNT(A.date) AS "days_vacant"
-FROM listings AS L
-JOIN availabilities AS A ON L.id = A.listing_id
-WHERE (A.date LIKE'2023-06-%')
-AND A.available = "TRUE"
-GROUP BY L.id;
