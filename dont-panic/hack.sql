1448d63172395053614cabfd3c8f81209f719d6b
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-24 02:44:59 +0000

    Mon, Jun 24, 2024, 5:44 AM +03:00

--- dont-panic/hack.sql
+++
@@ -1,8 +0,0 @@
-UPDATE "users" SET password = '982c0381c279d139fd221fce974916e7'
-WHERE "id" = 1;
-
-DELETE FROM user_logs WHERE old_username = 'admin';
-
-INSERT INTO user_logs( "type", "old_username", "new_username", "old_password", "new_password")
-SELECT 'update', 'admin', 'admin', NULL,( SELECT "password" FROM users
-WHERE username = 'emily33');
