0df22fbdaa470effa398e91884acfcb057a478a2
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-24 06:24:44 +0000

    Mon, Jun 24, 2024, 9:24 AM +03:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -11,7 +11,20 @@ CREATE TABLE IF NOT EXISTS "meteorite" (
     CONSTRAINT meteorite_pk PRIMARY KEY ("id")
 );
 
+CREATE TABLE IF NOT EXISTS "meteorites_temp"(
+"name" TEXT, "id" TEXT, "nametype" TEXT, "class" TEXT,
+ "mass" TEXT, "discovery" TEXT, "year" TEXT, "lat" TEXT,
+ "long" TEXT);
+
 INSERT INTO "meteorite" ("id", "name" , "class" , "mass", "discovery", "year", "lat", "long")
 SELECT "id", "name", "class", "mass", "discovery", "year", "lat", "long" FROM "meteorites_temp";
 
 
+INSERT INTO "meteorite" ("id", "name" , "class" , "mass", "discovery", "year", "lat", "long")
+SELECT "id", "name", "class", "mass", "discovery", "year", "lat", "long" FROM "meteorites_temp";
+
+
+sqlite> INSERT INTO "meteorite" ("id", "name" , "class" , "mass", "discovery", "year", "lat", "long")
+   ...> SELECT "id", "name", "class", "mass", "discovery", "year", "lat", "long" FROM "meteorites_temp";
+Parse error: table meteorite has no column named name
+sqlite> ^C
