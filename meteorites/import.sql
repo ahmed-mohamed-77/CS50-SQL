6ec3c9b33aa1d96dcaa25b28b9df47c78eb2b7fc
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-24 19:15:20 +0000

    Mon, Jun 24, 2024, 10:15 PM +03:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -15,19 +15,7 @@ CREATE TABLE IF NOT EXISTS "meteorite_temp" (
 
 .import --csv --skip 1 meteorites.csv meteorite_temp
 
-CREATE TABLE IF NOT EXISTS "meteorite" (
-    "id" INTEGER,
-    "name" VARCHAR(50) NOT NULL,
-    "class" VARCHAR(15) NOT NULL,
-    "mass" REAL ,
-    "discovery" NOT NULL CHECK("discovery" IN ('Fell', 'Found')),
-    "year" REAL,
-    "lat" REAL,
-    "long" REAL,
-    CONSTRAINT meteorite_pk PRIMARY KEY ("id")
-);
 
-SE
 
 UPDATE  "meteorite_temp"
 SET "lat" = ROUND("lat", 2),
@@ -46,7 +34,21 @@ SET "lat" = NULLIF("lat", 0),
     "long" = NULLIF("long", 0),
     "mass" = NULLIF("mass", 0);
 
-SELECT * FROM meteorite_temp WHERE
+SELECT * FROM meteorite_temp WHERE "lat" = 0 OR"long"= 0 OR "mass"= 0 ;
+SELECT * FROM meteorite_temp WHERE "lat"= '' OR "long"= '' OR "mass"= ''OR "year" = '';
+
+
+CREATE TABLE IF NOT EXISTS "meteorite" (
+    "id" INTEGER,
+    "name" VARCHAR(50) NOT NULL,
+    "class" VARCHAR(15) NOT NULL,
+    "mass" REAL ,
+    "discovery" NOT NULL CHECK("discovery" IN ('Fell', 'Found')),
+    "year" REAL,
+    "lat" REAL,
+    "long" REAL,
+    CONSTRAINT meteorite_pk PRIMARY KEY ("id")
+);
 
 
 INSERT INTO "meteorite" ("id", "name", "class", "mass", "discovery", "year", "lat", "long")
