2dc09bd88eb6eee3b24577cbf8a3f737cc2cb2ea
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-24 06:16:45 +0000

    Mon, Jun 24, 2024, 9:16 AM +03:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -1,13 +1,3 @@
-id, which represents the unique ID of the meteorite.
-name, which represents the given name of the meteorite.
-class, which is the classification of the meteorite, according to the traditional classification scheme.
-mass, which is the weight of the meteorite, in grams.
-discovery, which is either “Fell” or “Found”. “Fell” indicates the meteorite was seen falling to Earth, whereas “Found” indicates the meteorite was found only after landing on Earth.
-year, which is the year in which the the meteorite was discovered.
-lat, which is the latitude at which the meteorite landed.
-long, which is the longitude at which the meteorite landed.
-
-
 CREATE TABLE IF NOT EXISTS "meteorite" (
     "id" INTEGER
     "name" VARCHAR(50) NOT NULL,
@@ -16,5 +6,7 @@ CREATE TABLE IF NOT EXISTS "meteorite" (
     "discovery" VARCHAR(15) NOT NULL,
     "year" REAL NOT NULL,
     "lat" REAL NOT NULL,
-    "long" REAL NOT NULL
-)
+    "long" REAL NOT NULL,
+
+    CONSTRAINT meteorite_pk PRIMARY KEY ("id")
+);
