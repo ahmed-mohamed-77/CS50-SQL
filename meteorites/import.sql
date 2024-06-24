bb52a1d5dc85646946f4dbef690efc3e1927843c
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-24 06:45:26 +0000

    Mon, Jun 24, 2024, 9:45 AM +03:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -3,14 +3,13 @@ CREATE TABLE IF NOT EXISTS "meteorite" (
     "name" VARCHAR(50) NOT NULL,
     "class" VARCHAR(15) NOT NULL,
     "mass" REAL NOT NULL,
-    "discovery" VARCHAR(15) NOT NULL,
+    "discovery" NOT NULL CHECK("discovery" IN ('Fell', 'Found')),
     "year" REAL NOT NULL,
     "lat" REAL NOT NULL,
     "long" REAL NOT NULL,
     CONSTRAINT meteorite_pk PRIMARY KEY ("id")
 );
 
-discovery, which is either ('Fell', 'Found')
 
 INSERT INTO "meteorite" ("id", "name", "class", "mass", "discovery", "year", "lat", "long")
 SELECT
