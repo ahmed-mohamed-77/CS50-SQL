e77ba89021b1eb67ca9ef6226a7e81c73928100c
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-24 21:52:36 +0000

    Tue, Jun 25, 2024, 12:52 AM +03:00

--- bnb/frequently_reviewed.sql
+++ bnb/frequently_reviewed.sql
@@ -8,3 +8,32 @@ SELECT
     COUNT(R.id)
 FROM "listings" AS L
 JOIN "
+
+
+CREATE TABLE IF NOT EXISTS "listings" (
+    "id" INTEGER,
+    "property_type" TEXT,
+    "host_name" TEXT,
+    "accommodates" INTEGER,
+    "bedrooms" INTEGER,
+    "description" TEXT,
+    PRIMARY KEY("id")
+);
+CREATE TABLE IF NOT EXISTS "availabilities" (
+    "id" INTEGER,
+    "listing_id" INTEGER,
+    "date" NUMERIC,
+    "available" INTEGER,
+    "price" NUMERIC,
+    PRIMARY KEY("id"),
+    FOREIGN KEY("listing_id") REFERENCES "listings"("id")
+);
+CREATE TABLE IF NOT EXISTS "reviews" (
+    "id" INTEGER,
+    "listing_id" INTEGER,
+    "date" NUMERIC,
+    "reviewer_name" TEXT,
+    "comments" TEXT,
+    PRIMARY KEY("id"),
+    FOREIGN KEY("listing_id") REFERENCES "listings"("id")
+);
