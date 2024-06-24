aa4643349cb5b11af0b55e2b76c3e08cdb7c0269
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-24 06:27:57 +0000

    Mon, Jun 24, 2024, 9:27 AM +03:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -17,14 +17,13 @@ CREATE TABLE IF NOT EXISTS "meteorites_temp"(
  "long" TEXT);
 
 INSERT INTO "meteorite" ("id", "name" , "class" , "mass", "discovery", "year", "lat", "long")
-SELECT "id", "name", "class", "mass", "discovery", "year", "lat", "long" FROM "meteorites_temp";
-
-
-INSERT INTO "meteorite" ("id", "name" , "class" , "mass", "discovery", "year", "lat", "long")
-SELECT "id", "name", "class", "mass", "discovery", "year", "lat", "long" FROM "meteorites_temp";
-
-
-sqlite> INSERT INTO "meteorite" ("id", "name" , "class" , "mass", "discovery", "year", "lat", "long")
-   ...> SELECT "id", "name", "class", "mass", "discovery", "year", "lat", "long" FROM "meteorites_temp";
-Parse error: table meteorite has no column named name
-sqlite> ^C
+SELECT
+    CAST("id" AS INTEGER),
+    "name",
+    "class",
+    CAST("mass" AS REAL),
+    "discovery",
+    CAST("year" AS REAL),
+    CAST("lat" AS REAL),
+    CAST("long" AS REAL)
+FROM "meteorites_temp";
