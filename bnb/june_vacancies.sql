6cab3430875fb966b81cc03a5afcc17828cf3af6
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-25 12:54:40 +0000

    Tue, Jun 25, 2024, 3:54 PM +03:00

--- bnb/june_vacancies.sql
+++ bnb/june_vacancies.sql
@@ -9,9 +9,3 @@ JOIN "availabilities" ON  availabilities.listing_id =  listings.id
 WHERE date LIKE '2023-06-%'
 GROUP BY listings.id;
 
-SELECT
-    *
-FROM "listings"
-JOIN "availabilities" ON  availabilities.listing_id =  listings.id
-WHERE date LIKE '2023-06-%'
-GROUP BY listings.id;
