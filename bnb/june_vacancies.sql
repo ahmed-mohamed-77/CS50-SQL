8b06e14833a00bea4359912a255496b6660c77a4
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-25 12:30:58 +0000

    Tue, Jun 25, 2024, 3:30 PM +03:00

--- bnb/june_vacancies.sql
+++ bnb/june_vacancies.sql
@@ -1,9 +1,9 @@
 CREATE VIEW "june_vacancies" AS
 SELECT
     L.id,
-    L.property_type,
-    L.host_name,
-    COUNT(A.date) 
+    property_type,
+    host_name,
+    COUNT(A.date)
 FROM "listings" AS L
 JOIN "availabilities" AS A ON L.id = A.listing_id
 WHERE A.date LIKE '2023-06-%'
