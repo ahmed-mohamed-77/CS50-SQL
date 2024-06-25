14b12fc9646f43eac831ec8d6dd6f5d772de76a7
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-25 12:02:20 +0000

    Tue, Jun 25, 2024, 3:02 PM +03:00

--- bnb/june_vacancies.sql
+++ bnb/june_vacancies.sql
@@ -1,15 +1,10 @@
 CREATE VIEW "june_vacancies" AS
 SELECT
-    L.id
-    L.property_type
-    L.host_name
-    days_vacant
-
-
-June Vacancies
-In june_vacancies.sql, write a SQL statement to create a view named june_vacancies. This view should contain all listings and the number of days in June of 2023 that they remained vacant. Ensure the view contains the following columns:
-
-id, which is the id of the listing from the listings table.
-property_type, from the listings table.
-host_name, from the listings table.
-days_vacant, which is the number of days in June of 2023, that the given listing was marked as available.
+    L.id,
+    L.property_type,
+    L.host_name,
+    COUNT(A.date) AS days_vacant
+FROM "listings" AS L
+LEFT JOIN "availabilities" AS A ON L.id = A.listing_id
+WHERE A.date BETWEEN '2023-06-01' AND '2023-06-30' AND A.available = 1
+GROUP BY L.id, L.property_type, L.host_name;
