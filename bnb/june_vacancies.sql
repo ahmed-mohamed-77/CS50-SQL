014b7f60c1dfa709a9943bd32d3d3702794ab21b
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-25 12:04:38 +0000

    Tue, Jun 25, 2024, 3:04 PM +03:00

--- bnb/june_vacancies.sql
+++ bnb/june_vacancies.sql
@@ -6,5 +6,5 @@ SELECT
     COUNT(A.date) AS days_vacant
 FROM "listings" AS L
 LEFT JOIN "availabilities" AS A ON L.id = A.listing_id
-WHERE A.date BETWEEN '2023-06-01' AND '2023-06-30' AND A.available = 1
-GROUP BY L.id, L.property_type, L.host_name;
+WHERE A.date BETWEEN '2023-06-01' AND '2023-06-30'
+GROUP BY L.id;
