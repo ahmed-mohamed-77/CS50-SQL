d938fee72d9b600a2c54fad980077259feb80f0e
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-25 13:02:16 +0000

    Tue, Jun 25, 2024, 4:02 PM +03:00

--- bnb/june_vacancies.sql
+++ bnb/june_vacancies.sql
@@ -6,14 +6,5 @@ SELECT
     COUNT(availabilities.date)
 FROM "listings"
 JOIN "availabilities" ON  availabilities.listing_id =  listings.id
-WHERE date LIKE '2023-06-%'
+WHERE date LIKE '2023-06-%' AND availabilities.available = 'TRUE'
 GROUP BY listings.id;
-
-June Vacancies
-In june_vacancies.sql, write a SQL statement to create a view named june_vacancies.
-This view should contain all listings and the number of days in June of 2023 that they remained vacant. Ensure the view contains the following columns:
-
-id, which is the id of the listing from the listings table.
-property_type, from the listings table.
-host_name, from the listings table.
-days_vacant, which is the number of days in June of 2023, that the given listing was marked as available.
