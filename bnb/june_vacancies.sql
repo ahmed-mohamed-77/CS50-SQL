d173d464b5af20f5f2f20fccf3d53da1160fdf15
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-25 12:47:49 +0000

    Tue, Jun 25, 2024, 3:47 PM +03:00

--- bnb/june_vacancies.sql
+++ bnb/june_vacancies.sql
@@ -8,3 +8,8 @@ FROM "listings"
 JOIN "availabilities" ON  availabilities.listing_id =  listings.id
 GROUP BY listings.id
 HAVING availabilities.date LIKE '2023-06-%';
+
+
+SELECT COUNT(*) FROM availabilities
+WHERE "date" LIKE '2023-06-%';
+
