3f2a786514d3c6f4074986b129be386ff262b237
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-25 19:37:48 +0000

    Tue, Jun 25, 2024, 10:37 PM +03:00

---
+++ private/private.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "triplet" (
+    "sentence_id" INTEGER,
+    "starting_character" INTEGER,
+    "length_character" INTEGER
+);
+
+INSERT INTO "triplet" ("sentence_id", "starting_character", "length_character") VALUES
+(14, 98, 4),
+(114, 3, 5),
+(618, 72, 9),
+(630, 7, 3),
+(932, 12, 5),
+(2230, 50, 7),
+(2346, 44, 10),
+(3041, 14, 5);
