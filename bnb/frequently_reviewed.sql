259b1946468cc69bd2b75b7c67d72e11c6d254b4
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-25 11:51:53 +0000

    Tue, Jun 25, 2024, 2:51 PM +03:00

--- bnb/frequently_reviewed.sql
+++ bnb/frequently_reviewed.sql
@@ -1,39 +1,11 @@
-
-
-
+CREATE VIEW "frequently_reviewed" AS
 SELECT
     L.id,
     L.property_type,
     L.host_name,
-    COUNT(R.id)
+    COUNT(R.id) AS reviews
 FROM "listings" AS L
-JOIN "
-
-
-CREATE TABLE IF NOT EXISTS "listings" (
-    "id" INTEGER,
-    "property_type" TEXT,
-    "host_name" TEXT,
-    "accommodates" INTEGER,
-    "bedrooms" INTEGER,
-    "description" TEXT,
-    PRIMARY KEY("id")
-);
-CREATE TABLE IF NOT EXISTS "availabilities" (
-    "id" INTEGER,
-    "listing_id" INTEGER,
-    "date" NUMERIC,
-    "available" INTEGER,
-    "price" NUMERIC,
-    PRIMARY KEY("id"),
-    FOREIGN KEY("listing_id") REFERENCES "listings"("id")
-);
-CREATE TABLE IF NOT EXISTS "reviews" (
-    "id" INTEGER,
-    "listing_id" INTEGER,
-    "date" NUMERIC,
-    "reviewer_name" TEXT,
-    "comments" TEXT,
-    PRIMARY KEY("id"),
-    FOREIGN KEY("listing_id") REFERENCES "listings"("id")
-);
+JOIN "reviews" AS R ON L.id = R.listing_id
+GROUP BY L.id, L.property_type, L.host_name
+ORDER BY reviews DESC, L.property_type, L.host_name
+LIMIT 100;
