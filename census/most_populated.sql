f40cc302a5d4fd06bd3a80a277b83de04327f630
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-27 08:05:32 +0000

    Thu, Jun 27, 2024, 11:05 AM +03:00

--- census/most_populated.sql
+++ census/most_populated.sql
@@ -7,6 +7,15 @@ SELECT
     COUNT("male") AS "Male",
     COUNT("female") AS "Female"
 FROM "census"
-WHERE "locality" LIKE '%Nepal%'
 GROUP BY "district"
+HAVING "locality" LIKE '%Nepal%'
 ORDER BY "district" DESC;
+Most Populated
+In most_populated.sql, write a SQL statement to create a view named most_populated. This view should contain, in order from greatest to least, the most populated districts in Nepal. Ensure the view contains each of the following columns:
+
+district, which is the name of the district.
+families, which is the total number of families in the district.
+households, which is the total number of households in the district.
+population, which is the total population of the district.
+male, which is the total number of people identifying as male in the district.
+female, which is the total number of people identifying as female in the district.
