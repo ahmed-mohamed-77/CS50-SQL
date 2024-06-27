e609354c449cd4f54c6547e5ed5081f78906b1ee
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-27 08:11:27 +0000

    Thu, Jun 27, 2024, 11:11 AM +03:00

--- census/most_populated.sql
+++ census/most_populated.sql
@@ -14,3 +14,16 @@ ORDER BY "population" DESC;
 
 SELECT * FROM "census"
 WHERE "locality" LIKE '%Nepal%' OR "locality" LIKE '%nepal%';
+
+
+SELECT
+    "district",
+    SUM("families") AS "families",
+    SUM("households") AS "households",
+    SUM("population") AS "population",
+    SUM("male") AS "male",
+    SUM("female") AS "female"
+FROM "census"
+WHERE "locality" = 'Nepal'
+GROUP BY "district"
+ORDER BY "population" DESC;
