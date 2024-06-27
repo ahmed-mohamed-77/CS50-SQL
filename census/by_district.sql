27850e568695684597833defc1d9c4916dd26ee1
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-27 08:02:03 +0000

    Thu, Jun 27, 2024, 11:02 AM +03:00

---
+++ census/by_district.sql
@@ -0,0 +1,10 @@
+CREATE VIEW "by_district" AS
+SELECT
+    "district",
+    SUM("families") AS "Families",
+    SUM("households") AS "Households",
+    SUM("population") AS "Population",
+    SUM("male") AS "Male",
+    SUM("female") AS "Female"
+FROM "census"
+GROUP BY "district";
