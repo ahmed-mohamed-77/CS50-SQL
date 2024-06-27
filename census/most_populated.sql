a89fc64d32ce237a8f500f33d5f36e555dc3989a
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-27 08:16:11 +0000

    Thu, Jun 27, 2024, 11:16 AM +03:00

--- census/most_populated.sql
+++ census/most_populated.sql
@@ -7,24 +7,6 @@ SELECT
     SUM("male") AS "male",
     SUM("female") AS "female"
 FROM "census"
-WHERE "locality" LIKE '%Nepal%'
-GROUP BY "district"
-ORDER BY "population" DESC;
-
-
-SELECT * FROM "census"
-WHERE "district" = 'Kathmandu';
-
-
-SELECT
-    "district",
-    SUM("families") AS "families",
-    SUM("households") AS "households",
-    SUM("population") AS "population",
-    SUM("male") AS "male",
-    SUM("female") AS "female"
-FROM "census"
-WHERE "district" = 'Kathmandu'
 GROUP BY "district"
 ORDER BY "population" DESC;
 
