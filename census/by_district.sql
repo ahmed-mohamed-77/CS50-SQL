e89e35b4649e8b1722bea33ed3e51d4697cb0e85
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-06-27 08:24:25 +0000

    Thu, Jun 27, 2024, 11:24 AM +03:00

--- census/by_district.sql
+++ census/by_district.sql
@@ -1,24 +1,16 @@
 CREATE VIEW "by_district" AS
 SELECT
     "district",
-    SUM("families") AS "Families",
-    SUM("households") AS "Households",
-    SUM("population") AS "Population",
-    SUM("male") AS "Male",
-    SUM("female") AS "Female"
+    SUM("families") AS "families",
+    SUM("households") AS "households",
+    SUM("population") AS "population",
+    SUM("male") AS "male",
+    SUM("female") AS "female"
 FROM "census"
 GROUP BY "district"
-ORDER BY "Families" ASC;
+ORDER BY "families" ASC;
 
-
-
-By District
-In by_district.sql, write a SQL statement to create a view named by_district. This view should contain the sums for each numeric column in census, grouped by district. Ensure the view contains each of the following columns:
-
-district, which is the name of the district.
-families, which is the total number of families in the district.
-households, which is the total number of households in the district.
-population, which is the total population of the district.
-male, which is the total number of people identifying as male in the district.
-female, which is the total number of people identifying as female in the district.
-Which district has the second lowest number of families? And how many does it have? Using your by_district view, you should find that the Mustang district has only 3,751 families.
+SELECT "district", "families"
+FROM "by_district"
+ORDER BY "families" ASC
+LIMIT 1 OFFSET 1;
