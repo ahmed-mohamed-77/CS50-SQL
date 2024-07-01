7b4b13b8aa40d7a19ba9017765d4561ec1dd943d
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-07-01 16:16:51 +0000

    Mon, Jul 1, 2024, 7:16 PM +03:00

---
+++ snap/1.sql
@@ -0,0 +1,3 @@
+The app’s user engagement team needs to identify active users. Find all usernames of users who have logged in
+ since 2024-01-01. Ensure your query uses the search_users_by_last_login index, which is defined as follows:
+
