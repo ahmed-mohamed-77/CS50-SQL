efedb0f3c580d9fa1b892c09cf361ab88d40c8d6
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-07-01 16:21:36 +0000

    Mon, Jul 1, 2024, 7:21 PM +03:00

--- snap/1.sql
+++ snap/1.sql
@@ -1,4 +1,4 @@
 The app’s user engagement team needs to identify active users. Find all usernames of users who have logged in
  since 2024-01-01. Ensure your query uses the search_users_by_last_login index, which is defined as follows:
 
-select * from users where year(joined_date) = 2024;
+select * from users where strftime('%Y',joined_date) = 2024;
