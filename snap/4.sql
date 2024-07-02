33685bba0bc9f7336c6bf5e188f7aeba64add435
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-07-02 10:32:18 +0000

    Tue, Jul 2, 2024, 1:32 PM +03:00

--- snap/4.sql
+++ snap/4.sql
@@ -1,8 +1,6 @@
 SELECT
-    "users"."username",
-    "to_user_id",
-    COUNT("from_user_id") AS "messages_received"
+    "users"."username"
 FROM "messages"
 JOIN "users" ON "messages"."from_user_id" = "users"."id"
 GROUP BY "to_user_id"
-ORDER BY "messages_received" DESC;
+ORDER BY COUNT("from_user_id") DESC, "users"."username" ASC;
