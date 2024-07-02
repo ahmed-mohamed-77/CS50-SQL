ecc805620c46c269589a6c985b4ef3afe27d51f9
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-07-02 10:46:49 +0000

    Tue, Jul 2, 2024, 1:46 PM +03:00

--- snap/4.sql
+++ snap/4.sql
@@ -2,5 +2,6 @@ SELECT
     "users"."username"
 FROM "messages"
 JOIN "users" ON "messages"."from_user_id" = "users"."id"
-GROUP BY "to_user_id"
-ORDER BY COUNT("from_user_id") DESC, "users"."username" ASC;
+GROUP BY "from_user_id"
+ORDER BY COUNT("to_user_id") DESC, "users"."username" ASC;
+
