abec31a8873728073973c32efe55cc8871b50269
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-07-02 10:28:37 +0000

    Tue, Jul 2, 2024, 1:28 PM +03:00

--- snap/4.sql
+++ snap/4.sql
@@ -1,8 +1,12 @@
 SELECT
-    "from_user_id",
+    "users"."username",
     "to_user_id",
     COUNT("from_user_id") AS "messages_received"
 FROM "messages"
 JOIN "users" ON "messages"."from_user_id" = "users"."id"
 GROUP BY "to_user_id"
 ORDER BY "messages_received" DESC;
+The app needs to send users a summary of their engagement. Find the username of the most popular user, defined as the user who has had the most messages sent to them. If two or more users have had the same number of messages sent to them, return the username that is first alphabetically.
+
+Ensure your query uses the search_messages_by_to_user_id index, which is defined as follows:
+
