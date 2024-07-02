4812d3042d863d1fd299919f3494479c27c148cb
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-07-02 09:54:08 +0000

    Tue, Jul 2, 2024, 12:54 PM +03:00

--- snap/3.sql
+++ snap/3.sql
@@ -1,6 +1,7 @@
 
-SELECT * FROM  "messages"
+SELECT * , COUNT("to_user_id") FROM  "messages"
 WHERE "from_user_id" IN (
     SELECT id FROM users
     WHERE username = 'creativewisdom377'
-);
+)
+GROUP BY from_user_id;
