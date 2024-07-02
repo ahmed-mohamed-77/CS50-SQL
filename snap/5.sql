6fc3df6d7dfecb9941c10df5301a298ba8f517d0
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-07-02 10:56:04 +0000

    Tue, Jul 2, 2024, 1:56 PM +03:00

--- snap/5.sql
+++ snap/5.sql
@@ -1,1 +1,16 @@
 Given two usernames, "lovelytrust487" and "exceptionalinspiration482",
+SELECT
+    "from_user_id",
+    "to_user_id",
+WHERE "from_user_id" = (
+    SELECT "id" FROM "users"
+    WHERE "username" = 'lovelytrust487'
+)INTERSECT
+
+SELECT
+    "from_user_id",
+    "to_user_id",
+WHERE "from_user_id" = (
+    SELECT "id" FROM "users"
+    WHERE "username" = 'lovelytrust487'
+)
