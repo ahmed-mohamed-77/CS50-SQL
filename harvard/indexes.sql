94c75f4931becda9905e03a46688302c09b7a472
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-07-02 11:08:51 +0000

    Tue, Jul 2, 2024, 2:08 PM +03:00

--- harvard/indexes.sql
+++ harvard/indexes.sql
@@ -1,5 +1,6 @@
 EXPLAIN QUERY PLAN
-SELECT * FROM enrollments
-WHERE student_id = (
-    SELECT id FROM students WHERE id = 2
-);
+SELECT * FROM "enrollments" AS E
+JOIN courses AS C ON E.course_id = C.id
+WHERE "student_id" = (
+    SELECT "id" FROM "students" WHERE "id" = 2
+) ;
