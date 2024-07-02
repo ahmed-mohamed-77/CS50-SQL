f0e0faf3c745b20ab35788a9fa23c5751177f37e
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-07-02 10:23:25 +0000

    Tue, Jul 2, 2024, 1:23 PM +03:00

--- snap/4.sql
+++ snap/4.sql
@@ -1,10 +1,13 @@
 SELECT
     "id",
-    COUNT("from_user_id"),
+    "from_user_id",
     "to_user_id",
+    COUNT("from_user_id") AS "messages_received",
     "picture",
     "sent_timestamp",
     "viewed_timestamp",
     "expires_timestamp"
 FROM "messages"
-GROUP BY "to_user_id";
+GROUP BY "to_user_id"
+ORDER BY "messages_received" DESC
+LIMIT 3;
