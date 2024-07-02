d868ecab418c01cb3c6fe67d11325faed2b1c184
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-07-02 10:57:14 +0000

    Tue, Jul 2, 2024, 1:57 PM +03:00

--- snap/5.sql
+++ snap/5.sql
@@ -1,7 +1,9 @@
-Given two usernames, "lovelytrust487" and "exceptionalinspiration482",
-SELECT
+For any two users, the app needs to quickly show a list of the friends they have in common. Given two usernames, lovelytrust487 and exceptionalinspiration482, find the user IDs of their mutual friends. A mutual friend is a user that both lovelytrust487 and exceptionalinspiration482 count among their friends.
+
+Ensure your query uses the index automatically created on primary key columns of the friends table. This index is called sqlite_autoindex_friends_1.SELECT
     "from_user_id",
-    "to_user_id",
+    "to_user_id"
+FROM "messages"
 WHERE "from_user_id" = (
     SELECT "id" FROM "users"
     WHERE "username" = 'lovelytrust487'
@@ -9,7 +11,8 @@ WHERE "from_user_id" = (
 
 SELECT
     "from_user_id",
-    "to_user_id",
+    "to_user_id"
+FROM "messages"
 WHERE "from_user_id" = (
     SELECT "id" FROM "users"
     WHERE "username" = 'lovelytrust487'
