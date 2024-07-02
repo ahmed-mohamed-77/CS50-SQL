ab3680da13308772dcd766868e8d6e895d5beb7a
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-07-02 11:10:52 +0000

    Tue, Jul 2, 2024, 2:10 PM +03:00

--- harvard/indexes.sql
+++ harvard/indexes.sql
@@ -1,6 +1,1 @@
-EXPLAIN QUERY PLAN
-SELECT * FROM "enrollments" AS E
-JOIN courses AS C ON E.course_id = C.id
-WHERE "student_id" = (
-    SELECT "id" FROM "students" WHERE "id" = 2
-) ;
+CREATE INDEX "courses_info" ON "courses"("department", "title");
