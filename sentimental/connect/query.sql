dfbd6963482cf9cbd14ee80a5156d4269d4b1e0a
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-07-04 05:34:09 +0000

    Thu, Jul 4, 2024, 8:34 AM +03:00

---
+++ sentimental/connect/query.sql
@@ -0,0 +1,10 @@
+The heart of LinkedIn’s platform is its people. Your database should be able to represent the following information about LinkedIn’s users:
+
+Their first and last name
+Their username
+Their password
+
+
+CREATE TABLE IF NOT EXISTS `users`(
+    `id` INT AUTO_INCREMENT
+)
