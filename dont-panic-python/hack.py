9dafdade94a72a17b3fad7dac31e422800686962
Ahmed Mohamed <112273991+ahmed-mohamed-77@users.noreply.github.com>
2024-07-04 05:05:45 +0000

    Thu, Jul 4, 2024, 8:05 AM +03:00

--- dont-panic-python/hack.py
+++ dont-panic-python/hack.py
@@ -2,6 +2,13 @@ from cs50 import SQL
 
 
 db = SQL("sqlite:///dont-panic.db")
-
+password = input("Enter new password: ".title()).strip()
+db.execute(
+    """
+        UPDATE users SET password = ?
+        WHERE username = 'admin';
+    """,
+    password
+)
 
 print("Hacked!")
